8f0545ccb9a1982176072ce6f582670a07498b31
Tarrence van As <tarrence@vanas.family>
2022-10-25 11:05:50 -0400

    feat: use udc for address computation

--- keychain/src/methods/register.ts
+++ keychain/src/methods/register.ts
@@ -8,7 +8,7 @@ import Controller from "utils/account";
 const PROXY_CLASS = "0x793a374a266432184f68b29546d14fedfdcbe6346bc51bd34ad730e6ff914f3";
 const ACCOUNT_CLASS = "0x21a58754bd7658d29f70e1e5dbebf84ae393a5ef704c4f5a763cc8a61cb3414";
 const CONTROLLER_CLASS = "0x10baeb4233aae14d72f1c2f60d8c46be61436fb06631c835df93b3a9f566351";
-const ACCOUNT_ADDRESS = "0x07d7bbf672edd77578b8864c3e2900ac9194698220adb1b1ecdc45f9222ca291";
+const ACCOUNT_ADDRESS = "0x041a78e741e5af2fec34b695679bc6891742439f7afb8484ecd7766661ad02bf";
 
 const register = () => async (
   username: string, credential: { x: string, y: string }
