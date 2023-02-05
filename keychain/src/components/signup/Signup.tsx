49a86d107365d118a90f10205c9fb4d47c54a2d3
Tarrence van As <tarrence@vanas.family>
2023-02-05 05:22:23 -0500

    fix: gate deploy

--- keychain/src/components/signup/Signup.tsx
+++ keychain/src/components/signup/Signup.tsx
@@ -175,21 +175,23 @@ export const Signup = ({
           controller.account(constants.StarknetChainId.TESTNET).sync();
         });
 
-      controller.account(constants.StarknetChainId.MAINNET).status =
-        Status.DEPLOYING;
-      client
-        .request(DeployAccountDocument, {
-          id: debouncedName,
-          chainId: "starknet:SN_MAIN",
-          starterpackIds: starterData?.game?.starterPack?.chainID?.includes(
-            "SN_MAIN",
-          )
-            ? [starterData?.game?.starterPack?.id]
-            : undefined,
-        })
-        .then(() => {
-          controller.account(constants.StarknetChainId.MAINNET).sync();
-        });
+      if (controller.account(constants.StarknetChainId.MAINNET).status != Status.DEPLOYING) {
+        controller.account(constants.StarknetChainId.MAINNET).status =
+          Status.DEPLOYING;
+        client
+          .request(DeployAccountDocument, {
+            id: debouncedName,
+            chainId: "starknet:SN_MAIN",
+            starterpackIds: starterData?.game?.starterPack?.chainID?.includes(
+              "SN_MAIN",
+            )
+              ? [starterData?.game?.starterPack?.id]
+              : undefined,
+          })
+          .then(() => {
+            controller.account(constants.StarknetChainId.MAINNET).sync();
+          });
+      }
     },
     [keypair, starterData, debouncedName, onController],
   );
@@ -342,8 +344,8 @@ export const Signup = ({
                       canContinue
                         ? "green.400"
                         : nameError
-                        ? "red.400"
-                        : "gray.600"
+                          ? "red.400"
+                          : "gray.600"
                     }
                     errorBorderColor="crimson"
                     placeholder="Username"
