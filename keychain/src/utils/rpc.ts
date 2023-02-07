b966422eb9e29420dddc757286e70187cf93d698
broody <broody@gmail.com>
2023-02-07 11:00:35 -0800

    fix(keychain): getTxnReceipt returns not found

--- keychain/src/utils/rpc.ts
+++ keychain/src/utils/rpc.ts
@@ -1,12 +1,44 @@
-const FEEDER_ENDPOINT = "https://alpha4.starknet.io/feeder_gateway";
+import { constants } from "starknet";
+
+const GOERLI_ENDPOINT = "https://alpha4.starknet.io/feeder_gateway";
+const MAINNET_ENDPOINT = "https://alpha-mainnet.starknet.io/feeder_gateway";
 
 // TODO: this should probably be supported by starknetjs
-export const getClassByHash = async (hash: string): Promise<any> => {
-  const raw = await get("get_class_by_hash", `classHash=${hash}`);
+export const getClassByHash = async (
+  chainId: constants.StarknetChainId,
+  hash: string,
+): Promise<any> => {
+  const raw = await get(chainId, "get_class_by_hash", `classHash=${hash}`);
   return await raw.json();
 };
 
-function get(method: any, params: any): Promise<any> {
-  const url = `${FEEDER_ENDPOINT}/${method}?${params}`;
-  return fetch(url, { method: "GET" });
+export const getTransactionReceipt = async (
+  chainId: constants.StarknetChainId,
+  hash: string,
+): Promise<any> => {
+  const raw = await get(
+    chainId,
+    "get_transaction_receipt",
+    `transactionHash=${hash}`,
+  );
+  return await raw.json();
+};
+
+function get(
+  chainId: constants.StarknetChainId,
+  method: any,
+  params: any,
+): Promise<any> {
+  switch (chainId) {
+    case constants.StarknetChainId.MAINNET:
+      return fetch(`${MAINNET_ENDPOINT}/${method}?${params}`, {
+        method: "GET",
+      });
+    case constants.StarknetChainId.TESTNET:
+      return fetch(`${GOERLI_ENDPOINT}/${method}?${params}`, {
+        method: "GET",
+      });
+    default:
+      throw Error("chain not supported");
+  }
 }
