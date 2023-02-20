d21cb1d69ca4602337379ef69fafe2c8b97477c8
Sam Rogers <sam.0110.rogers@gmail.com>
2023-02-20 15:59:59 -0600

    feat(keychain): sign message UI (#132)

--- keychain/src/components/SignMessage.tsx
+++ keychain/src/components/SignMessage.tsx
@@ -1,17 +1,63 @@
 import { useEffect, useState } from "react";
-import { css } from "@emotion/react";
-import { Box, Spacer, Text, VStack } from "@chakra-ui/react";
+import { Circle, Flex, HStack, Spacer, Text, VStack } from "@chakra-ui/react";
 
 import { typedData as td, shortString, constants, Signature } from "starknet";
 
 import Container from "./Container";
-import { Banner } from "components/Banner";
 import Controller from "utils/controller";
 import Footer from "./Footer";
 import { ResponseCodes } from "@cartridge/controller";
 import { Error } from "@cartridge/controller/src/types";
-import { Status } from "utils/account";
 import { Header } from "./Header";
+import Transfer from "./icons/Transfer";
+
+const DataContainer = (
+  {
+    title,
+    children,
+  }: {
+    title: string,
+    children: React.ReactNode,
+  }
+) => {
+  return (
+    <Flex
+      direction="column"
+      align="start"
+      w="full"
+      css={{
+        "> p": {
+          width: "100%",
+          padding: "12px",
+          fontSize: "13px",
+          lineHeight: "16px",
+          backgroundColor: "var(--chakra-colors-gray-600)",
+        },
+        "> p:first-of-type": {
+          fontSize: "10px",
+          fontWeight: "700",
+          letterSpacing: "0.05em",
+          lineHeight: "18px",
+          textTransform: "uppercase",
+          color: "var(--chakra-colors-gray-200)",
+          backgroundColor: "var(--chakra-colors-gray-700)",
+          borderRadius: "6px 6px 0 0",
+        },
+        "> p:last-of-type": {
+          borderRadius: "0 0 6px 6px",
+        },
+        "> p:not(p:last-of-type)": {
+          borderBottom: "1px solid var(--chakra-colors-gray-800)",
+        }
+      }}
+    >
+      <>
+        <Text>{title}</Text>
+        {children}
+      </>
+    </Flex>
+  );
+}
 
 const SignMessage = ({
   controller,
@@ -28,7 +74,7 @@ const SignMessage = ({
   onSign: (sig: Signature) => void;
   onCancel: (error: Error) => void;
 }) => {
-  const [messageData, setMessageData] = useState({});
+  const [messageData, setMessageData] = useState<td.TypedData>();
 
   useEffect(() => {
     if (!typedData) return;
@@ -62,6 +108,7 @@ const SignMessage = ({
   return (
     <Container>
       <Header
+        address={controller.address}
         chainId={chainId}
         onClose={() =>
           onCancel({
@@ -70,23 +117,56 @@ const SignMessage = ({
           })
         }
       />
-      <Banner
-        title="Signature Request"
-        description={`${origin} is asking you to sign a message`}
-        chainId={chainId}
-        py="20px"
-      />
-      <VStack p="12px" bgColor="gray.600" borderRadius="5px" align="flex-start">
-        <Text
-          as="pre"
-          fontSize="11px"
-          color="gray.200"
-          whiteSpace="pre-wrap"
-          wordBreak="break-all"
-        >
-          {JSON.stringify(messageData, null, 2)}
-        </Text>
-      </VStack>
+      <Spacer minH="36px" />
+      <Flex
+        direction="column"
+        align="start"
+        gap="18px"
+        w="full"
+      >
+        <HStack w="full" pb="24px" borderBottom="1px solid" borderColor="gray.700">
+          <Circle bgColor="gray.700" size="40px">
+            <Transfer color="green.400" fontSize="30px" />
+          </Circle>
+          <VStack align="start">
+            <Text fontSize="17px" lineHeight="20px" fontWeight="600">Signature Request</Text>
+            <Text color="gray.200" fontSize="12px" lineHeight="16px">{origin} is asking you to sign a message</Text>
+          </VStack>
+        </HStack>
+        {(() => {
+          if (!messageData) return <></>;
+          const ptName = messageData.primaryType;
+          const pt = messageData.types[ptName];
+          const values = (typeName: string) => {
+            const v = messageData.message[typeName];
+            if (typeof v === "object") {
+              return Object.entries(v).map(([key, value]) => {
+                return (
+                  <Text key={key}>
+                    <Text
+                      as="span"
+                      opacity="50%"
+                      textTransform="capitalize"
+                    >
+                      {key}:
+                    </Text> {value}
+                  </Text>
+                )
+              })
+            } else {
+              return <Text>{v as string}</Text>;
+            }
+          }
+
+          return pt.map((typ) => {
+            return (
+              <DataContainer key={typ.name} title={typ.name}>
+                {values(typ.name)}
+              </DataContainer>
+            );
+          });
+        })()}
+      </Flex>
 
       <Footer
         onConfirm={async () => {
