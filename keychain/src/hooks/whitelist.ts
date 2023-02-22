601a9350c54448f2f9f70ac55cc23d65e4a4f64e
broody <broody@gmail.com>
2023-02-22 07:54:39 -0800

    Revert "temp hide signup"
    This reverts commit 4553792de8e783e909e1ab6f0fcb7fb554bb1642.

--- keychain/src/hooks/whitelist.ts
+++ keychain/src/hooks/whitelist.ts
@@ -31,7 +31,7 @@ export const useWhitelist = (): {
       }
 
       if (domain.hostname.endsWith("cartridge.gg")) {
-        setSignupEnabled(false);
+        setSignupEnabled(true);
         setWeb3AuthEnabled(false);
         return;
       }
