60fffa614962670705a7b4fd5526c55b98cc902f
Junichi Sugiura <jun.sugiura.jp@gmail.com>
2023-09-19 17:46:34 +0300

    Add world explorer logo (#176)
    * Add world explorer logo
    
    * Reset chakra body background
    
    * Set IBM Plex Mono as default button font

--- packages/ui/src/theme/components/Button.tsx
+++ packages/ui/src/theme/components/Button.tsx
@@ -11,7 +11,7 @@ export const Button: ComponentStyleConfig = defineStyleConfig({
   baseStyle: {
     outline: "none",
     borderRadius: "base",
-    fontFamily: "IBM Plex Mono",
+    fontFamily: "var(--font-ibm-plex-mono)",
     fontWeight: "semibold",
     ":hover:disabled": {
       cursor: "not-allowed",
