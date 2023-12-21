ba25a4983cf7cfdb54cd8a76d76396d711fda93c
broody <broody@gmail.com>
2023-12-21 13:46:45 -1000

    force url

--- examples/starknet-react-next/src/pages/_app.tsx
+++ examples/starknet-react-next/src/pages/_app.tsx
@@ -4,12 +4,7 @@ import { StarknetProvider } from "@starknet-react/core";
 import CartridgeConnector from "@cartridge/connector";
 import { RpcProvider } from "starknet";
 
-const url =
-  process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
-    ? `https://keychain-git-${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}.preview.cartridge.gg`
-        .replace("(", "")
-        .replace(")/", "-") // e.g. `feat(keychain)/branch-name` -> `featkeychain-branch-name`
-    : process.env.XFRAME_URL;
+const url = "https://keychain.preview.cartridge.gg/";
 const connectors = [
   new CartridgeConnector([{ target: "0xdeadbeef", method: "testMethod" }], {
     url,
