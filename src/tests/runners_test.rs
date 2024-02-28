a8f851f34074ff29296ac3b007ea3a0c9cac0bcb
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-02-28 11:02:29 +0000

    remove devnet runner test

--- src/tests/runners_test.rs
+++ src/tests/runners_test.rs
@@ -1,11 +1,6 @@
-use super::runners::{devnet_runner::DevnetRunner, katana_runner::KatanaRunner, TestnetRunner};
+use super::runners::{katana_runner::KatanaRunner, TestnetRunner};
 
 #[test]
 fn test_katana_runner() {
     KatanaRunner::load();
-}
-
-#[test]
-fn test_devnet_runner() {
-    DevnetRunner::load();
 }
