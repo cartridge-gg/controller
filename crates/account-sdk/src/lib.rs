031b65dc626d4a3b4a4a466ba413e54d68394616
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-03-11 18:31:49 +0100

    init account-sdk example

---
+++ crates/account-sdk/src/lib.rs
@@ -0,0 +1,18 @@
+mod utils;
+
+use account_sdk::webauthn_signer::P256r1Signer;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, account-sdk!");
+    let signer = P256r1Signer::random(origin.clone());
+    let private_key = SigningKey::from_random();
+
+    alert(format!("{}", private_key.verifying_key().scalar()));
+}
