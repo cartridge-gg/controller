24faf3160787dc3f68786e634a8dbaf10fa8587d
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-04-05 17:23:56 +0200

    Remove vite build

--- packages/ui-next/.storybook/main.ts
+++ packages/ui-next/.storybook/main.ts
@@ -10,7 +10,7 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/stories/**/*.mdx", "../src/stories/**/*.stories.@(ts|tsx)"],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
