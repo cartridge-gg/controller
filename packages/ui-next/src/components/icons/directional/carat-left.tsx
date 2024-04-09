3bc4c9edcb81b0d41ced41560a14fc07a4c68ba4
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-04-09 21:28:05 +0200

    Port directional icons

--- packages/ui-next/src/components/icons/directional/carat-left.tsx
+++
@@ -1,8 +0,0 @@
-import { memo } from "react";
-import { Icon, IconProps } from "@chakra-ui/react";
-
-export const CaratLeftIcon = memo((props: IconProps) => (
-  <Icon viewBox="0 0 24 24" {...props}>
-    <path fill="currentColor" d="m13.6 16-4-4 4-4h.8v8h-.8Z" />
-  </Icon>
-));
