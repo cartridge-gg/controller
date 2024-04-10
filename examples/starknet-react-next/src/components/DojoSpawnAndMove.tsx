da6044078adf898785081153a8fa488aaeaaef5e
broody <broody@gmail.com>
2024-04-10 10:07:58 -1000

    Update starknet-react example

--- examples/starknet-react-next/src/components/DojoSpawnAndMove.tsx
+++ examples/starknet-react-next/src/components/DojoSpawnAndMove.tsx
@@ -1,4 +1,5 @@
 import { useAccount, useContractWrite } from "@starknet-react/core";
+import { useEffect, useState } from "react";
 
 const worldAddress =
   "0x426bf51e62109e5c98e7d36dddf813881b610a1ff7c241e51ec3533ce58778c";
@@ -7,7 +8,8 @@ const actionsAddress =
 
 export function DojoSpawnAndMove() {
   const { account } = useAccount();
-  const { write: spawn } = useContractWrite({
+  const [txnHash, setTxnHash] = useState<string>();
+  const { writeAsync: spawn } = useContractWrite({
     calls: [
       {
         contractAddress: actionsAddress,
@@ -17,7 +19,7 @@ export function DojoSpawnAndMove() {
     ],
   });
 
-  const { write: move } = useContractWrite({
+  const { writeAsync: move } = useContractWrite({
     calls: [
       {
         contractAddress: actionsAddress,
@@ -36,8 +38,36 @@ export function DojoSpawnAndMove() {
       <h2>Spawn And Move (Dojo example)</h2>
       <p>Actions Address: {actionsAddress}</p>
       <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
-        <button onClick={() => spawn()}>Spawn</button>
-        <button onClick={() => move()}>Move Left</button>
+        <button
+          onClick={async () => {
+            setTxnHash(undefined);
+            const { transaction_hash } = await spawn();
+            setTxnHash(transaction_hash);
+          }}
+        >
+          Spawn
+        </button>
+        <button
+          onClick={async () => {
+            setTxnHash(undefined);
+            const { transaction_hash } = await move();
+            setTxnHash(transaction_hash);
+          }}
+        >
+          Move Left
+        </button>
+        {txnHash && (
+          <p>
+            Transaction hash:{" "}
+            <a
+              href={`https://sepolia.starkscan.io/tx/${txnHash}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {txnHash}
+            </a>
+          </p>
+        )}
       </div>
     </>
   );
