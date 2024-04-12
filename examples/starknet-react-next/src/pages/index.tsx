33350c4ef7ed334e7946b1e897ae34eb6c64b5c5
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-04-12 15:56:28 +0200

    Fix preview keychain url

--- examples/starknet-react-next/src/pages/index.tsx
+++ examples/starknet-react-next/src/pages/index.tsx
@@ -8,20 +8,6 @@ import { DojoSpawnAndMove } from "components/DojoSpawnAndMove";
 const Home: NextPage = () => {
   return (
     <div>
-      <div>
-        <p>VERCEL_ENV: {process.env.NEXT_PUBLIC_VERCEL_ENV}</p>
-        <p>VERCEL_URL: {process.env.NEXT_PUBLIC_VERCEL_URL}</p>
-        <p>VERCEL_BRANCH_URL: {process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}</p>
-        <p>
-          Keychain URL: https://
-          {process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
-            ? (process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL as string).replace(
-                "cartridge-starknet-react-next-git",
-                "keychain-git",
-              )
-            : process.env.XFRAME_URL}
-        </p>
-      </div>
       <h2>Wallet</h2>
       <ConnectWallet />
       <DojoSpawnAndMove />
