38b8422fa89ea5357db2ee4805420daf656137f1
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-05-07 15:00:33 +0000

    guardian implementation

--- src/account.rs
+++ src/account.rs
@@ -145,3 +145,129 @@ where
         self.block_id
     }
 }
+
+pub struct CartridgeGuardianAccount<P, S, G>
+where
+    P: Provider + Send,
+    S: AccountSigner + Send,
+    G: AccountSigner + Send,
+{
+    account: CartridgeAccount<P, S>,
+    guardian: G,
+}
+
+impl<P, S, G> CartridgeGuardianAccount<P, S, G>
+where
+    P: Provider + Send,
+    S: AccountSigner + Send,
+    G: AccountSigner + Send,
+{
+    pub fn new(
+        provider: P,
+        signer: S,
+        guardian: G,
+        address: FieldElement,
+        chain_id: FieldElement,
+    ) -> Self {
+        CartridgeGuardianAccount {
+            account: CartridgeAccount::new(provider, signer, address, chain_id),
+            guardian,
+        }
+    }
+    pub fn from_account(account: CartridgeAccount<P, S>, guardian: G) -> Self {
+        Self { account, guardian }
+    }
+}
+
+impl<P, S, G> ExecutionEncoder for CartridgeGuardianAccount<P, S, G>
+where
+    P: Provider + Send,
+    S: AccountSigner + Send,
+    G: AccountSigner + Send,
+{
+    fn encode_calls(&self, calls: &[Call]) -> Vec<FieldElement> {
+        self.account.encode_calls(calls)
+    }
+}
+
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+impl<P, S, G> Account for CartridgeGuardianAccount<P, S, G>
+where
+    P: Provider + Send + Sync,
+    S: AccountSigner + Send + Sync,
+    G: AccountSigner + Send + Sync,
+{
+    type SignError = SignError;
+
+    fn address(&self) -> FieldElement {
+        self.account.address()
+    }
+
+    fn chain_id(&self) -> FieldElement {
+        self.account.chain_id()
+    }
+
+    async fn sign_execution(
+        &self,
+        execution: &RawExecution,
+        query_only: bool,
+    ) -> Result<Vec<FieldElement>, Self::SignError> {
+        let tx_hash = execution.transaction_hash(self.chain_id(), self.address(), query_only, self);
+        let owner_signature = self.account.signer.sign(&tx_hash).await.ok().unwrap();
+        let guardian_signature = self.guardian.sign(&tx_hash).await.ok().unwrap();
+        Ok(Vec::<SignerSignature>::cairo_serialize(&vec![
+            owner_signature,
+            guardian_signature,
+        ]))
+    }
+
+    async fn sign_declaration(
+        &self,
+        _declaration: &RawDeclaration,
+        _query_only: bool,
+    ) -> Result<Vec<FieldElement>, Self::SignError> {
+        unimplemented!("sign_declaration")
+    }
+
+    async fn sign_legacy_declaration(
+        &self,
+        _legacy_declaration: &RawLegacyDeclaration,
+        _query_only: bool,
+    ) -> Result<Vec<FieldElement>, Self::SignError> {
+        unimplemented!("sign_legacy_declaration")
+    }
+
+    fn execute(&self, calls: Vec<Call>) -> Execution<Self> {
+        Execution::new(calls, self)
+    }
+
+    fn declare(
+        &self,
+        contract_class: Arc<FlattenedSierraClass>,
+        compiled_class_hash: FieldElement,
+    ) -> Declaration<Self> {
+        Declaration::new(contract_class, compiled_class_hash, self)
+    }
+
+    fn declare_legacy(&self, contract_class: Arc<LegacyContractClass>) -> LegacyDeclaration<Self> {
+        LegacyDeclaration::new(contract_class, self)
+    }
+}
+
+impl<P, S, G> ConnectedAccount for CartridgeGuardianAccount<P, S, G>
+where
+    P: Provider + Send + Sync,
+    S: AccountSigner + Send + Sync,
+    G: AccountSigner + Send + Sync,
+{
+    type Provider = P;
+
+    fn provider(&self) -> &Self::Provider {
+        &self.account.provider()
+    }
+
+    fn block_id(&self) -> BlockId {
+        self.account.block_id()
+    }
+}
