c1ef0b5b9a56e39cbb6573d555401ee857a742e2
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-05-08 08:59:39 +0000

    add execution signer

--- src/signers/mod.rs
+++ src/signers/mod.rs
@@ -27,6 +27,36 @@ pub trait TransactionHashSigner {
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
 #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
 pub trait TransactionExecutionSigner {
-    async fn sign(&self, execution: RawExecution, tx_hash: &FieldElement) -> Result<SignerSignature, SignError>;
+    async fn sign(&self, execution: &RawExecution, tx_hash: &FieldElement) -> Result<SignerSignature, SignError>;
     fn signer(&self) -> Signer;
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+#[async_trait]
+impl<T> TransactionExecutionSigner for T
+where
+    T: TransactionHashSigner + Send + Sync,
+{
+    async fn sign(&self, _execution: &RawExecution, tx_hash: &FieldElement) -> Result<SignerSignature, SignError> {
+        self.sign(tx_hash).await
+    }
+
+    fn signer(&self) -> Signer {
+        self.signer()
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+#[async_trait(?Send)]
+impl<T> TransactionExecutionSigner for T
+where
+    T: TransactionHashSigner,
+{
+    async fn sign(&self, _execution: &RawExecution, tx_hash: &FieldElement) -> Result<SignerSignature, SignError> {
+        self.sign(tx_hash).await
+    }
+
+    fn signer(&self) -> Signer {
+        self.signer()
+    }
 }
