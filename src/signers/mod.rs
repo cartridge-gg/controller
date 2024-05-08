f7be488d56d686defca7b5e4eba3b051080835c3
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-05-08 09:05:10 +0000

    remove unnecessary

--- src/signers/mod.rs
+++ src/signers/mod.rs
@@ -1,7 +1,7 @@
 pub mod starknet;
 pub mod webauthn;
 
-use ::starknet::{accounts::RawExecution, core::crypto::EcdsaSignError};
+use ::starknet::core::crypto::EcdsaSignError;
 use starknet_crypto::FieldElement;
 
 use crate::abigen::cartridge_account::{Signer, SignerSignature};
@@ -22,41 +22,4 @@ pub enum SignError {
 pub trait TransactionHashSigner {
     async fn sign(&self, tx_hash: &FieldElement) -> Result<SignerSignature, SignError>;
     fn signer(&self) -> Signer;
-}
-
-#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
-#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
-pub trait TransactionExecutionSigner {
-    async fn sign(&self, execution: &RawExecution, tx_hash: &FieldElement) -> Result<SignerSignature, SignError>;
-    fn signer(&self) -> Signer;
-}
-
-#[cfg(not(target_arch = "wasm32"))]
-#[async_trait]
-impl<T> TransactionExecutionSigner for T
-where
-    T: TransactionHashSigner + Send + Sync,
-{
-    async fn sign(&self, _execution: &RawExecution, tx_hash: &FieldElement) -> Result<SignerSignature, SignError> {
-        self.sign(tx_hash).await
-    }
-
-    fn signer(&self) -> Signer {
-        self.signer()
-    }
-}
-
-#[cfg(target_arch = "wasm32")]
-#[async_trait(?Send)]
-impl<T> TransactionExecutionSigner for T
-where
-    T: TransactionHashSigner,
-{
-    async fn sign(&self, _execution: &RawExecution, tx_hash: &FieldElement) -> Result<SignerSignature, SignError> {
-        self.sign(tx_hash).await
-    }
-
-    fn signer(&self) -> Signer {
-        self.signer()
-    }
 }
