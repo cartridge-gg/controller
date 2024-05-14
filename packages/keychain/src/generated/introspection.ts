9147c232fba73dd2fd318eb5d5809a873fa1aaed
broody <broody@gmail.com>
2024-05-14 09:54:51 -1000

    pnpm format

--- packages/keychain/src/generated/introspection.ts
+++ packages/keychain/src/generated/introspection.ts
@@ -1,25 +1,14 @@
-
-      export interface PossibleTypesResultData {
-        possibleTypes: {
-          [key: string]: string[]
-        }
-      }
-      const result: PossibleTypesResultData = {
-  "possibleTypes": {
-    "Attribute": [
-      "NumberAttribute",
-      "StringAttribute"
-    ],
-    "ContractMetadata": [
-      "ERC20Metadata",
-      "ERC721Metadata"
-    ],
-    "DeploymentConfig": [
-      "KatanaConfig",
-      "MadaraConfig",
-      "ToriiConfig"
-    ],
-    "Node": [
+export interface PossibleTypesResultData {
+  possibleTypes: {
+    [key: string]: string[];
+  };
+}
+const result: PossibleTypesResultData = {
+  possibleTypes: {
+    Attribute: ["NumberAttribute", "StringAttribute"],
+    ContractMetadata: ["ERC20Metadata", "ERC721Metadata"],
+    DeploymentConfig: ["KatanaConfig", "MadaraConfig", "ToriiConfig"],
+    Node: [
       "Account",
       "AccountQuest",
       "AccountStarterPack",
@@ -47,13 +36,9 @@
       "Token",
       "Transaction",
       "TransactionReceipt",
-      "TwitterQuest"
+      "TwitterQuest",
     ],
-    "TransactionMetadata": [
-      "Call",
-      "MultiCall"
-    ]
-  }
+    TransactionMetadata: ["Call", "MultiCall"],
+  },
 };
-      export default result;
-    
+export default result;
