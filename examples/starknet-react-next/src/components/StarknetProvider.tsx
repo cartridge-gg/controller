d41f80b017723875b6f035b7985b334dc1594f5e
broody <broody@gmail.com>
2024-05-22 14:18:52 -1000

    Fix safari login/signup

--- examples/starknet-react-next/src/components/StarknetProvider.tsx
+++ examples/starknet-react-next/src/components/StarknetProvider.tsx
@@ -25,16 +25,16 @@ export function StarknetProvider({ children }: PropsWithChildren) {
   );
 }
 
-const url =
-  !process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL ||
-  process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL.split(".")[0] ===
-    "cartridge-starknet-react-next"
-    ? process.env.XFRAME_URL
-    : "https://" +
-      (process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL ?? "").replace(
-        "cartridge-starknet-react-next",
-        "keychain",
-      );
+const url = "https://keychain-git-safari-test.preview.cartridge.gg";
+  // !process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL ||
+  // process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL.split(".")[0] ===
+  //   "cartridge-starknet-react-next"
+  //   ? process.env.XFRAME_URL
+  //   : "https://" +
+  //     (process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL ?? "").replace(
+  //       "cartridge-starknet-react-next",
+  //       "keychain",
+  //     );
 
 const connectors = [
   new CartridgeConnector(
