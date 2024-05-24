c78d259d4a06b5ef959872d2ab9112fb4acdf2b4
Junichi Sugiura <jun.sugiura.jp@gmail.com>
2024-05-24 19:14:49 +0200

    Add more theme presets (#304)

--- packages/controller/src/presets.ts
+++ packages/controller/src/presets.ts
@@ -1,23 +1,37 @@
 import { ControllerThemePreset } from "./types";
 
-const cartridge: ControllerThemePreset = {
-  id: "cartridge",
-  name: "Cartridge",
-  icon: "/whitelabel/cartridge/icon.svg",
-  cover: "/whitelabel/cartridge/cover.png",
-};
-
-const rollyourown: ControllerThemePreset = {
-  id: "rollyourown",
-  name: "Roll Your Own",
-  icon: "/whitelabel/ryo/icon.png",
-  cover: "/whitelabel/ryo/cover.png",
-  colors: {
-    primary: "#11ED83",
+export const defaultPresets: { [key: string]: ControllerThemePreset } = {
+  cartridge: {
+    id: "cartridge",
+    name: "Cartridge",
+    icon: "/whitelabel/cartridge/icon.svg",
+    cover: "/whitelabel/cartridge/cover-dark.png",
   },
-};
-
-export const defaultPresets = {
-  cartridge,
-  rollyourown,
+  "force-prime": {
+    id: "force-prime",
+    name: "Force Prime",
+    icon: "/whitelabel/force-prime/icon.png",
+    cover: "/whitelabel/force-prime/cover.png",
+    colors: {
+      primary: "#E1CC89",
+    },
+  },
+  paved: {
+    id: "paved",
+    name: "Paved",
+    icon: "/whitelabel/paved/icon.svg",
+    cover: "/whitelabel/paved/cover.png",
+    colors: {
+      primary: "#B0CAF8",
+    },
+  },
+  rollyourown: {
+    id: "rollyourown",
+    name: "Roll Your Own",
+    icon: "/whitelabel/rollyourown/icon.png",
+    cover: "/whitelabel/rollyourown/cover.png",
+    colors: {
+      primary: "#11ED83",
+    },
+  },
 } as const;
