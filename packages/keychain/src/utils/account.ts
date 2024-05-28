6acb794682ce82dd2d752340c01c2952104ea4c8
broody <broody@gmail.com>
2024-05-28 10:51:02 -1000

    Use actual estimated fees

--- packages/keychain/src/utils/account.ts
+++ packages/keychain/src/utils/account.ts
@@ -15,6 +15,7 @@ import {
   waitForTransactionOptions,
   TransactionFinalityStatus,
   InvocationsDetails,
+  num,
 } from "starknet";
 import {
   AccountContractDocument,
@@ -175,9 +176,7 @@ class Account extends BaseAccount {
 
     transactionsDetail.nonce =
       transactionsDetail.nonce ?? (await this.getNonce("pending"));
-    // FIXME: estimated max fee is always too low
-    //transactionsDetail.maxFee = num.toHex(transactionsDetail.maxFee);
-    transactionsDetail.maxFee = "0x38D7EA4C68000"; // 0.001 eth
+    transactionsDetail.maxFee = num.toHex(transactionsDetail.maxFee);
 
     const res = await this.cartridge
       .execute(calls as Array<Call>, transactionsDetail, session)
