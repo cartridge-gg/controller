6747aabb258b5b0bbc55f9d171f7296d512b0e38
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-06-14 17:12:54 +0200

    Prevent long list of session details to push down footer buttons

--- packages/keychain/src/components/layout/Footer/SessionDetails.tsx
+++ packages/keychain/src/components/layout/Footer/SessionDetails.tsx
@@ -1,23 +1,10 @@
-import {
-  HStack,
-  VStack,
-  Text,
-  AccordionButton,
-  AccordionItem,
-  Accordion,
-  Spacer,
-  AccordionPanel,
-} from "@chakra-ui/react";
-import { CodeUtilIcon, WedgeRightIcon } from "@cartridge/ui";
+import { VStack } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { usePolicies } from "hooks/connection";
-import { PORTAL_WINDOW_HEIGHT, useLayout } from "../Container";
-import { isIframe } from "components/connect/utils";
-import { TOP_BAR_HEIGHT } from "../Container/Header/TopBar";
+import { Policies } from "Policies";
 
 export function SessionDetails() {
   const policies = usePolicies();
-  const { footerHeight } = useLayout();
 
   return (
     <VStack
@@ -36,69 +23,7 @@ export function SessionDetails() {
         display: "flex",
       }}
     >
-      <VStack align="flex-start" bg="solid.primary" p={3}>
-        <Text
-          color="text.secondaryAccent"
-          fontSize="xs"
-          fontWeight="bold"
-          casing="uppercase"
-        >
-          Session details
-        </Text>
-      </VStack>
-
-      <Accordion
-        w="full"
-        allowMultiple
-        overflowY="auto"
-        maxH={
-          (isIframe() ? window.innerHeight : PORTAL_WINDOW_HEIGHT) -
-          TOP_BAR_HEIGHT -
-          footerHeight
-        }
-      >
-        {policies.map((p, i) => (
-          <AccordionItem
-            key={p.target + p.method}
-            // The container already set border radius (for top & bottom), but we
-            // set the bottom radius for the last item here because for certain
-            // browsers' scrolling behaviour (eg Firefox) just to make it look nicer.
-            borderBottomRadius={i === policies.length - 1 ? "md" : "none"}
-            isDisabled={!p.description}
-          >
-            {({ isExpanded }) => (
-              <>
-                <AccordionButton
-                  _disabled={{
-                    cursor: "auto",
-                    opacity: 1,
-                  }}
-                >
-                  <HStack>
-                    <CodeUtilIcon boxSize={4} />
-                    <Text>{p.method}</Text>
-                  </HStack>
-
-                  <Spacer />
-
-                  {p.description && (
-                    <WedgeRightIcon
-                      fontSize="2xl"
-                      transform={isExpanded ? "rotate(90deg)" : undefined}
-                      transition="all 0.2s ease"
-                      color="text.secondary"
-                    />
-                  )}
-                </AccordionButton>
-
-                {p.description && (
-                  <AccordionPanel>{p.description}</AccordionPanel>
-                )}
-              </>
-            )}
-          </AccordionItem>
-        ))}
-      </Accordion>
+      <Policies title="Session Details" policies={policies} />
     </VStack>
   );
 }
