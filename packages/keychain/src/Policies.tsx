c10853caf575df7cde3cf42c549451eee46f6f64
Junichi Sugiura <jun.sugiura.jp@gmail.com>
2024-06-14 17:17:27 +0200

    Prevent long list of session details to push down footer buttons (#371)

---
+++ packages/keychain/src/Policies.tsx
@@ -0,0 +1,87 @@
+import {
+  HStack,
+  VStack,
+  Text,
+  AccordionButton,
+  AccordionItem,
+  Accordion,
+  Spacer,
+  AccordionPanel,
+  Box,
+} from "@chakra-ui/react";
+import { CodeUtilIcon, WedgeRightIcon } from "@cartridge/ui";
+import { Policy } from "@cartridge/controller";
+
+export function Policies({
+  title,
+  policies,
+}: {
+  title: string;
+  policies: Policy[];
+}) {
+  return (
+    <Box position="relative">
+      <VStack
+        align="flex-start"
+        bg="solid.primary"
+        p={3}
+        position="sticky"
+        top={0}
+        zIndex={1}
+      >
+        <Text
+          color="text.secondaryAccent"
+          fontSize="xs"
+          fontWeight="bold"
+          casing="uppercase"
+        >
+          {title}
+        </Text>
+      </VStack>
+
+      <Accordion w="full" allowMultiple overflowY="auto">
+        {policies.map((p, i) => (
+          <AccordionItem
+            key={p.target + p.method}
+            // The container already set border radius (for top & bottom), but we
+            // set the bottom radius for the last item here because for certain
+            // browsers' scrolling behaviour (eg Firefox) just to make it look nicer.
+            borderBottomRadius={i === policies.length - 1 ? "md" : "none"}
+            isDisabled={!p.description}
+          >
+            {({ isExpanded }) => (
+              <>
+                <AccordionButton
+                  _disabled={{
+                    cursor: "auto",
+                    opacity: 1,
+                  }}
+                >
+                  <HStack>
+                    <CodeUtilIcon boxSize={4} />
+                    <Text>{p.method}</Text>
+                  </HStack>
+
+                  <Spacer />
+
+                  {p.description && (
+                    <WedgeRightIcon
+                      fontSize="2xl"
+                      transform={isExpanded ? "rotate(90deg)" : undefined}
+                      transition="all 0.2s ease"
+                      color="text.secondary"
+                    />
+                  )}
+                </AccordionButton>
+
+                {p.description && (
+                  <AccordionPanel>{p.description}</AccordionPanel>
+                )}
+              </>
+            )}
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </Box>
+  );
+}
