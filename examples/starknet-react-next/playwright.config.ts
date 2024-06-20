b459a2bfcaad02e246ba4eb7cabab8207133947a
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-06-20 17:34:56 +0200

    Setup Playwright

---
+++ examples/starknet-react-next/playwright.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    baseURL: "http://localhost:3002",
+    trace: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
+  ],
+  webServer: {
+    command: "cd ../../ && pnpm dev",
+    url: "http://localhost:3002",
+    reuseExistingServer: !process.env.CI,
+  },
+});
