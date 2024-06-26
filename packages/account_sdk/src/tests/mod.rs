bdbb931e471fdb3173bc17e93896210fac6c0c1d
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-06-26 14:24:19 +0000

    add external owners test

--- packages/account_sdk/src/tests/mod.rs
+++ packages/account_sdk/src/tests/mod.rs
@@ -3,12 +3,12 @@ mod declaration_using_account_test;
 mod deployment_test;
 mod ecdsa_test;
 mod erc20_methods_test;
+mod external_owners_test;
 mod guardian;
 mod outside_execution_test;
+mod register_session_test;
 mod runners;
 mod runners_test;
 mod session;
 mod signers;
 mod stark_pair_auth_test;
-
-mod register_session_test;
