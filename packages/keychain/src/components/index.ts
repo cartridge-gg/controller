acb1aab12ef5f0a18dce2dbf81a7f018a9e995e2
broody <broody@gmail.com>
2024-07-01 07:47:35 -1000

    Remove deploying state and cleanup

--- packages/keychain/src/components/index.ts
+++ packages/keychain/src/components/index.ts
@@ -2,5 +2,4 @@ export * from "./Animation";
 export * from "./DeploymentRequired";
 export * from "./Execute";
 export * from "./Loading";
-export * from "./Redeploy";
 export * from "./SignMessage";
