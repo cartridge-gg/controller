c27dddf915c9288e3c010fdc875a0ff4e8f51a43
broody <broody@gmail.com>
2024-07-25 07:02:22 -1000

    bump controller storage version

--- examples/starknet-react-next/src/components/StarknetProvider.tsx
+++ examples/starknet-react-next/src/components/StarknetProvider.tsx
@@ -18,16 +18,7 @@ export function StarknetProvider({ children }: PropsWithChildren) {
   );
 }
 
-const url =
-  !process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL ||
-  process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL.split(".")[0] ===
-    "cartridge-starknet-react-next"
-    ? process.env.XFRAME_URL
-    : "https://" +
-      (process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL ?? "").replace(
-        "cartridge-starknet-react-next",
-        "keychain",
-      );
+const url = "https://keychain-git-argent-comp-1.preview.cartridge.gg"
 
 function provider(chain: Chain) {
   switch (chain) {
@@ -76,7 +67,7 @@ const connectors = [
     ],
     {
       url,
-      rpc: process.env.NEXT_PUBLIC_RPC_MAINNET,
+      rpc: process.env.NEXT_PUBLIC_RPC_SEPOLIA,
       // paymaster: {
       //   caller: shortString.encodeShortString("ANY_CALLER"),
       // },
