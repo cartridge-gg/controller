9a40ec673287c90698ae9313e24114c85dba80f3
Tarrence van As <tarrence@vanas.family>
2024-08-06 13:04:58 -0400

    Build wasm

--- packages/account-wasm/pkg/account_wasm_bg.js
+++ packages/account-wasm/pkg/account_wasm_bg.js
@@ -999,7 +999,7 @@ export function __wbindgen_memory() {
     return addHeapObject(ret);
 };
 
-export function __wbindgen_closure_wrapper2212(arg0, arg1, arg2) {
+export function __wbindgen_closure_wrapper2202(arg0, arg1, arg2) {
     const ret = makeMutClosure(arg0, arg1, 490, __wbg_adapter_48);
     return addHeapObject(ret);
 };
