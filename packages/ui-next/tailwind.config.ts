8fb66fd91d722d9c9b08c730638f6a7e44928d61
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-08-07 13:34:33 +0200

    Expose tailwind preset as separated submodule for node

--- packages/ui-next/tailwind.config.ts
+++ packages/ui-next/tailwind.config.ts
@@ -1,10 +1,9 @@
 import type { Config } from "tailwindcss";
-import { cartridgeTWPreset } from "./src";
+import { cartridgeTWPreset } from "./src/preset";
 
 const config = {
   darkMode: "class",
   content: ["./src/**/*.{ts,tsx}", "./.storybook/**/*.{ts,tsx}"],
-  prefix: "",
   presets: [cartridgeTWPreset],
 } satisfies Config;
 
