1c824b1e3966ce3eb9b97323e0d3909a972d5d84
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-08-13 15:22:20 +0200

    Show keychain url in example for debugging

--- examples/starknet-react-next/src/app/page.tsx
+++ examples/starknet-react-next/src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { TransferEth } from "components/TransferEth";
 import { ConnectWallet } from "components/ConnectWallet";
 import { InvalidTxn } from "components/InvalidTxn";
@@ -5,8 +7,15 @@ import { SignMessage } from "components/SignMessage";
 import { DelegateAccount } from "components/DelegateAccount";
 import { ColorModeToggle } from "components/ColorModeToggle";
 import { Menu } from "components/Menu";
+import { KEYCHAIN_URL } from "components/providers/StarknetProvider";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [debug, setDebug] = useState(false);
+  useEffect(() => {
+    setDebug(!!new URLSearchParams(window.location.search).get("debug"))
+  }, [])
+
   return (
     <div className="flex flex-col p-4 gap-4">
       <div className="flex justify-between">
@@ -15,6 +24,16 @@ export default function Home() {
         </h2>
         <ColorModeToggle />
       </div>
+
+      {debug && (
+        <div>
+          <p>Keychain URL: {KEYCHAIN_URL}</p>
+          <p>NEXT_PUBLIC_VERCEL_ENV: {String(process.env.NEXT_PUBLIC_VERCEL_ENV)}</p>
+          <p>NEXT_PUBLIC_VERCEL_BRANCH_URL: {String(process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL)}</p>
+          <p>NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL: {String(process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL)}</p>
+        </div>
+      )}
+
       <ConnectWallet />
       <Menu />
       <TransferEth />
