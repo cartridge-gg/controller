68c4fb0d5e72d254a10e10723ddcf34201d6a261
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-08-29 13:27:42 +0200

    Replace address in menu with CopyAddress comp

--- packages/keychain/src/components/Menu/index.tsx
+++ packages/keychain/src/components/Menu/index.tsx
@@ -1,49 +1,16 @@
-import { Button, VStack, Text, HStack, Spacer } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 import { Container, Content, Footer } from "components/layout";
-import { CopyIcon } from "@cartridge/ui";
+import { CopyAddress } from "components/CopyAddress";
 import { useConnection } from "hooks/connection";
-import { useState } from "react";
-import { formatAddress } from "utils/contracts";
 
 export function Menu({ onLogout }: { onLogout: () => void }) {
   const { controller } = useConnection();
-  const [isCopying, setIsCopying] = useState(false);
-
-  const onCopy = () => {
-    setIsCopying(true);
-    navigator.clipboard.writeText(controller.address);
-    setTimeout(() => {
-      setIsCopying(false);
-    }, 1_000);
-  };
-
   return (
     <Container
       variant="menu"
       showSettings={true}
       title={controller.username}
-      description={
-        <VStack
-          cursor="pointer"
-          _hover={{
-            color: "color.primary",
-          }}
-          onClick={onCopy}
-          alignItems="flex-start"
-        >
-          {!isCopying ? (
-            <HStack>
-              <Text>
-                {formatAddress(controller.address, { first: 6, last: 4 })}
-              </Text>
-              <Spacer />
-              <CopyIcon color="currentColor" />
-            </HStack>
-          ) : (
-            <Text color="text.secondary">Copied to clipboard!</Text>
-          )}
-        </VStack>
-      }
+      description={<CopyAddress address={controller.address} />}
     >
       <Content h="350px"></Content>
       <Footer>
