e684ed1a18e093237a42f4882437adec233ae90e
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-09-02 18:44:15 -0400

    move tests to tests folder

--- packages/account_sdk/src/tests/mod.rs
+++ packages/account_sdk/src/tests/mod.rs
@@ -12,12 +12,14 @@ use crate::transaction_waiter::{TransactionWaiter, TransactionWaitingError};
 pub(crate) mod account;
 pub(crate) mod runners;
 
+mod controller_test;
 mod declare_test;
 mod delegate_account_test;
 mod external_owners_test;
 mod guardian_test;
 mod outside_execution_test;
 mod owner_test;
+mod paymaster_test;
 mod register_session_test;
 mod session_test;
 
