90942dba35d01e837aca6581a677f7fe04796316
Tarrence van As <tarrencev@users.noreply.github.com>
2024-09-09 22:00:55 -0400

    Update CI Rust to 1.80.0 (#677)

--- packages/account_sdk/src/constants.rs
+++ packages/account_sdk/src/constants.rs
@@ -7,8 +7,8 @@ pub const ACCOUNT_CLASS_HASH: Felt =
 pub const ACCOUNT_COMPILED_CLASS_HASH: Felt =
     felt!("0x6b22de13b878ab346fa53442adaa8a40a6bd25732aa1aeb2a26375987f0be00");
 
-pub const ERC_20_COMPILED_CLASS_HASH: Felt =
-    felt!("0x732654ca6baa90ff202d2fcc35fb39766eb34842a7e5ac6dbf7714af71f1dab");
-
 pub const ETH_CONTRACT_ADDRESS: Felt =
     felt!("0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7");
+
+pub const ERC_20_COMPILED_CLASS_HASH: Felt =
+    felt!("0x732654ca6baa90ff202d2fcc35fb39766eb34842a7e5ac6dbf7714af71f1dab");
