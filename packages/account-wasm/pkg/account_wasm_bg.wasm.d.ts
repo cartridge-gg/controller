1d7352fce437ccd0b992ca5420aeb3719427e348
Szymon Wojtulewicz <58044978+piniom@users.noreply.github.com>
2024-09-12 13:48:49 -0400

    Added build script warning to the auto-generated file (#704)
    * added build script warning to the auto-generated file
    
    * remove eth constant

--- packages/account-wasm/pkg/account_wasm_bg.wasm.d.ts
+++ packages/account-wasm/pkg/account_wasm_bg.wasm.d.ts
@@ -31,8 +31,8 @@ export function __wbg_set_jscontrollererror_data(a: number, b: number, c: number
 export function __wbindgen_malloc(a: number, b: number): number;
 export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
 export const __wbindgen_export_2: WebAssembly.Table;
-export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8842d5b7e89cfff4(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hab75a3c637f96005(a: number, b: number, c: number): void;
 export function __wbindgen_add_to_stack_pointer(a: number): number;
 export function __wbindgen_free(a: number, b: number, c: number): void;
 export function __wbindgen_exn_store(a: number): void;
-export function wasm_bindgen__convert__closures__invoke2_mut__h06f5a6b5a9886359(a: number, b: number, c: number, d: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h47b629288504f118(a: number, b: number, c: number, d: number): void;
