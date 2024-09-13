5a5baadbf4d5cc36c840e2dfd625cabc09b780ef
Yun <broody@gmail.com>
2024-09-12 20:41:37 -1000

    Remove comments (#711)

--- examples/starknet-react-next/src/components/providers/StarknetProvider.tsx
+++ examples/starknet-react-next/src/components/providers/StarknetProvider.tsx
@@ -22,8 +22,6 @@ export function StarknetProvider({ children }: PropsWithChildren) {
 
 const ETH_TOKEN_ADDRESS =
   "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
-// const PAPER_TOKEN_ADDRESS =
-//   "0x0410466536b5ae074f7fea81e5533b8134a9fa08b3dd077dd9db08f64997d113";
 
 const cartridge = new CartridgeConnector({
   policies: [
@@ -66,16 +64,6 @@ const cartridge = new CartridgeConnector({
   },
   // theme: "dope-wars",
   // colorMode: "light"
-  // prefunds: [
-  //   {
-  //     address: ETH_TOKEN_ADDRESS,
-  //     min: "300000000000000",
-  //   },
-  //   {
-  //     address: PAPER_TOKEN_ADDRESS,
-  //     min: "100",
-  //   },
-  // ],
 });
 
 function provider(chain: Chain) {
