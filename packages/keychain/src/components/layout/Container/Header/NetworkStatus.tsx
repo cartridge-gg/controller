01c6d5ee5ebe133559220867bec16bd519c9cf20
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-09-13 13:10:48 -0400

    Click to copy chain id to clipboard

--- packages/keychain/src/components/layout/Container/Header/NetworkStatus.tsx
+++ packages/keychain/src/components/layout/Container/Header/NetworkStatus.tsx
@@ -4,39 +4,51 @@ import {
   SlotIcon,
   AlertIcon,
 } from "@cartridge/ui";
-import { Circle, HStack, Text } from "@chakra-ui/react";
+import { Button, Circle } from "@chakra-ui/react";
 import { useConnection } from "hooks/connection";
+import { useToast } from "hooks/toast";
 import { constants } from "starknet";
 import { isSlotChain } from "utils/network";
 
 export function NetworkStatus() {
   const { chainName, chainId } = useConnection();
+  const { toast } = useToast();
 
   if (!chainName) {
     return;
   }
 
   return (
-    <HStack bg="solid.bg" borderRadius="base" p={3}>
-      <Circle size={5} bg="solid.primary">
-        {(() => {
-          switch (chainId) {
-            case constants.StarknetChainId.SN_MAIN:
-              return <StarknetColorIcon fontSize="xl" />;
-            case constants.StarknetChainId.SN_SEPOLIA:
-              return <StarknetIcon fontSize="xl" />;
-            default:
-              return isSlotChain(chainId) ? (
-                <SlotIcon fontSize="xl" />
-              ) : (
-                <AlertIcon fontSize="xl" />
-              );
-          }
-        })()}
-      </Circle>
-      <Text fontSize="xs" fontWeight={600} textTransform="uppercase">
-        {chainName}
-      </Text>
-    </HStack>
+    <Button
+      size="sm"
+      bg="solid.primary"
+      fontSize="xs"
+      fontFamily="Inter"
+      _hover={{ bg: "solid.primary", opacity: 0.7 }}
+      leftIcon={
+        <Circle size={5} bg="solid.primary">
+          {(() => {
+            switch (chainId) {
+              case constants.StarknetChainId.SN_MAIN:
+                return <StarknetColorIcon fontSize="xl" />;
+              case constants.StarknetChainId.SN_SEPOLIA:
+                return <StarknetIcon fontSize="xl" />;
+              default:
+                return isSlotChain(chainId) ? (
+                  <SlotIcon fontSize="xl" />
+                ) : (
+                  <AlertIcon fontSize="xl" />
+                );
+            }
+          })()}
+        </Circle>
+      }
+      onClick={() => {
+        navigator.clipboard.writeText(chainId);
+        toast("Chain ID is copied");
+      }}
+    >
+      {chainName}
+    </Button>
   );
 }
