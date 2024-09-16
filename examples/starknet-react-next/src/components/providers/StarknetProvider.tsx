78f183d82cd227914676be114adb5d03136d967b
Luca Steeb <contact@luca-steeb.com>
2024-09-16 18:42:00 -0400

    Add alias assignment for Vercel preview deployments (#727)
    Co-authored-by: JunichiSugiura <jun.sugiura.jp@gmail.com>

--- examples/starknet-react-next/src/components/providers/StarknetProvider.tsx
+++ examples/starknet-react-next/src/components/providers/StarknetProvider.tsx
@@ -11,7 +11,7 @@ export function StarknetProvider({ children }: PropsWithChildren) {
     <StarknetConfig
       autoConnect
       chains={[sepolia]}
-      connectors={[cartridge]}
+      connectors={[controller]}
       explorer={starkscan}
       provider={provider}
     >
@@ -23,7 +23,7 @@ export function StarknetProvider({ children }: PropsWithChildren) {
 const ETH_TOKEN_ADDRESS =
   "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
 
-const cartridge = new CartridgeConnector({
+const controller = new CartridgeConnector({
   policies: [
     {
       target: ETH_TOKEN_ADDRESS,
@@ -49,15 +49,7 @@ const cartridge = new CartridgeConnector({
     },
   ],
   url:
-    !process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL ||
-    process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL.split(".")[0] ===
-      "cartridge-starknet-react-next"
-      ? process.env.XFRAME_URL
-      : "https://" +
-        (process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL ?? "").replace(
-          "cartridge-starknet-react-next",
-          "keychain",
-        ),
+    process.env.NEXT_PUBLIC_KEYCHAIN_DEPLOYMENT_URL ?? process.env.XFRAME_URL,
   rpc: process.env.NEXT_PUBLIC_RPC_SEPOLIA,
   paymaster: {
     caller: shortString.encodeShortString("ANY_CALLER"),
