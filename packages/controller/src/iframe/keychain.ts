8478e795a00cbf47414b7a0b0a508653c6c60df2
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-09-18 14:48:18 -0400

    Put back policies search param

--- packages/controller/src/iframe/keychain.ts
+++ packages/controller/src/iframe/keychain.ts
@@ -6,8 +6,10 @@ export class KeychainIFrame extends IFrame<Keychain> {
   constructor({
     url,
     paymaster,
+    policies,
     ...options
-  }: IFrameOptions<Keychain> & Pick<ControllerOptions, "paymaster">) {
+  }: IFrameOptions<Keychain> &
+    Pick<ControllerOptions, "paymaster" | "policies">) {
     const _url = new URL(url ?? KEYCHAIN_URL);
     if (paymaster) {
       _url.searchParams.set(
@@ -15,6 +17,13 @@ export class KeychainIFrame extends IFrame<Keychain> {
         encodeURIComponent(JSON.stringify(paymaster)),
       );
     }
+    if (policies) {
+      _url.searchParams.set(
+        "policies",
+        encodeURIComponent(JSON.stringify(policies)),
+      );
+    }
+
     super({
       ...options,
       id: "controller-keychain",
