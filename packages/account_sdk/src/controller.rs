a22611018229968f9be1a4583658010ee7f588ab
Tarrence van As <tarrence@vanas.family>
2024-09-20 12:24:10 -0400

    Remove paymaster preprocessing

--- packages/account_sdk/src/controller.rs
+++ packages/account_sdk/src/controller.rs
@@ -207,38 +207,7 @@ where
     pub async fn execute_from_outside_raw(
         &self,
         outside_execution: OutsideExecution,
-    ) -> Result<Felt, ControllerError> {
-        let signed = self
-            .sign_outside_execution(outside_execution.clone())
-            .await?;
-
-        let res = self
-            .provider()
-            .add_execute_outside_transaction(
-                outside_execution,
-                self.account.address,
-                signed.signature,
-            )
-            .await
-            .map_err(ControllerError::PaymasterError)?;
-
-        Ok(res.transaction_hash)
-    }
-
-    pub async fn execute_from_outside(
-        &self,
-        calls: Vec<Call>,
     ) -> Result<InvokeTransactionResult, ControllerError> {
-        let now = get_current_timestamp();
-
-        let outside_execution = OutsideExecution {
-            caller: OutsideExecutionCaller::Any.into(),
-            execute_after: 0,
-            execute_before: now + 600,
-            calls: calls.into_iter().map(|call| call.into()).collect(),
-            nonce: SigningKey::from_random().secret_scalar(),
-        };
-
         let signed = self
             .sign_outside_execution(outside_execution.clone())
             .await?;
@@ -256,6 +225,23 @@ where
         Ok(InvokeTransactionResult {
             transaction_hash: res.transaction_hash,
         })
+    }
+
+    pub async fn execute_from_outside(
+        &self,
+        calls: Vec<Call>,
+    ) -> Result<InvokeTransactionResult, ControllerError> {
+        let now = get_current_timestamp();
+
+        let outside_execution = OutsideExecution {
+            caller: OutsideExecutionCaller::Any.into(),
+            execute_after: 0,
+            execute_before: now + 600,
+            calls: calls.into_iter().map(|call| call.into()).collect(),
+            nonce: SigningKey::from_random().secret_scalar(),
+        };
+
+        self.execute_from_outside_raw(outside_execution).await
     }
 
     pub async fn estimate_invoke_fee(
