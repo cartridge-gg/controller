61a468db511ef45620b2d099c8e155da497389de
broody <broody@gmail.com>
2024-09-30 18:18:42 -1000

    Prompt if session is expired

--- packages/keychain/src/const.ts
+++ packages/keychain/src/const.ts
@@ -1,2 +1,2 @@
 export const CARTRIDGE_DISCORD_LINK = "https://discord.gg/cartridge";
-export const SESSION_EXPIRATION = 1727776800n;
+export const SESSION_EXPIRATION = BigInt(Math.floor(Date.now() / 1000) + 7 * 24 * 60 * 60); // 1 week
