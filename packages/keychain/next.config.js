72c7b9ab8cd26852b39817d43a4cab056b9ba704
Tarrence van As <tarrencev@users.noreply.github.com>
2024-10-02 17:33:34 -0400

    Update wasm build step to fingerpint account.wasm (#806)

--- packages/keychain/next.config.js
+++ packages/keychain/next.config.js
@@ -13,8 +13,8 @@ const nextConfig = {
     // Fixes `Error occurred prerendering page "/"`
     config.output.webassemblyModuleFilename =
       isServer && !dev
-        ? "../static/wasm/webauthn.wasm"
-        : "static/wasm/webauthn.wasm";
+        ? "../static/wasm/[modulehash].wasm"
+        : "static/wasm/[modulehash].wasm";
 
     // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
     config.experiments = { ...config.experiments, asyncWebAssembly: true };
