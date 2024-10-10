df449fc2e2a5c149688172f4c15ce3c284ffe3a1
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-10-10 17:46:32 +0200

    Add ercTransfer query hook to utils package

--- packages/profile/src/components/provider/index.tsx
+++ packages/profile/src/components/provider/index.tsx
@@ -1,13 +1,20 @@
+import { PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ColorSchemeProvider } from "./colorScheme";
 import { QueryParamsProvider } from "./query";
-import { PropsWithChildren } from "react";
 import { ConnectionProvider } from "./connection";
 
 export function Provider({ children }: PropsWithChildren) {
+  const queryClient = new QueryClient();
+
   return (
     <QueryParamsProvider>
       <ColorSchemeProvider defaultScheme="system">
-        <ConnectionProvider>{children}</ConnectionProvider>
+        <ConnectionProvider>
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </ConnectionProvider>
       </ColorSchemeProvider>
     </QueryParamsProvider>
   );
