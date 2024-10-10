ab33621fc598065d78bb1e69d0c26c6b3387fee9
broody <broody@gmail.com>
2024-10-10 07:16:25 -1000

    Update logos

--- packages/keychain/src/const.ts
+++ packages/keychain/src/const.ts
@@ -1,4 +1,4 @@
 export const CARTRIDGE_DISCORD_LINK = "https://discord.gg/cartridge";
 export const SESSION_EXPIRATION = BigInt(
-  Math.floor(Date.now() / 1000) + 7 * 24 * 60 * 60,
+  Math.floor(Date.now() / 1000) + 10 * 365 * 24 * 60 * 60,
 ); // 1 week
