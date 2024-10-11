56296d2c9d0cf6f6b26ee723b7a67245ff87ea3c
Junichi Sugiura <jun.sugiura.jp@gmail.com>
2024-10-11 18:15:03 +0200

    Update controller icon (#850)

--- packages/keychain/src/pages/_app.tsx
+++ packages/keychain/src/pages/_app.tsx
@@ -20,6 +20,22 @@ export default function Keychain({ Component, pageProps }: AppProps) {
       <NextHead>
         <title>Cartridge Controller</title>
 
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-48x48.png"
+          sizes="48x48"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Controller" />
+        <link rel="manifest" href="/site.webmanifest" />
+
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
