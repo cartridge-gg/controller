6d15968f07a6b9e6cf58f691c884ff5a5572cacb
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-10-16 20:30:02 +0200

    remove `Debug` derive on `Controller`

--- packages/account_sdk/src/factory.rs
+++ packages/account_sdk/src/factory.rs
@@ -14,7 +14,7 @@ use crate::{
     signers::{HashSigner, SignError, Signer},
 };
 
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub struct ControllerFactory {
     class_hash: Felt,
     chain_id: Felt,
