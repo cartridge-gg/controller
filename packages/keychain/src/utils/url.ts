16be431aaf4a7b03090d71081034f944170fc12c
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-10-21 11:54:01 +0200

    Add explorer link

--- packages/keychain/src/utils/url.ts
+++ packages/keychain/src/utils/url.ts
@@ -1,25 +1,3 @@
-import { constants } from "starknet";
-
-const BASE_URL = {
-  [constants.StarknetChainId.SN_MAIN]: "https://starkscan.co",
-  [constants.StarknetChainId.SN_SEPOLIA]: "https://testnet.starkscan.co",
-};
-
-export const StarkscanUrl = (chainId: constants.StarknetChainId) => ({
-  transaction: (hash: string, fragment?: string) =>
-    `${BASE_URL[chainId]}/tx/${hash}${fragment ? `#${fragment}` : ""}`,
-  contract: (address: string, fragment?: string) =>
-    `${BASE_URL[chainId]}/contract/${address}${fragment ? `#${fragment}` : ""}`,
-  message: (address: string, fragment?: string) =>
-    `${BASE_URL[chainId]}/message/${address}${fragment ? `#${fragment}` : ""}`,
-  block: (id: string, fragment?: string) =>
-    `${BASE_URL[chainId]}/block/${id}${fragment ? `#${fragment}` : ""}`,
-  event: (address: string, fragment?: string) =>
-    `${BASE_URL[chainId]}/event/${address}${fragment ? `#${fragment}` : ""}`,
-  class: (address: string, fragment?: string) =>
-    `${BASE_URL[chainId]}/class/${address}${fragment ? `#${fragment}` : ""}`,
-});
-
 export const PopupCenter = (
   url: string,
   title: string,
