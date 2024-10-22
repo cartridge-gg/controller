a7effcbae7ec28380084ecf8f5bd05ab67d2eb2b
bal7hazar <bal7hazar@proton.me>
2024-10-22 09:46:04 +0200

    ⚡️ Improve code

--- packages/ui-next/src/components/primitives/scroll-area.tsx
+++ packages/ui-next/src/components/primitives/scroll-area.tsx
@@ -9,43 +9,20 @@ const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
 >(({ className, children, ...props }, ref) => {
-  const [hidden, setHidden] = React.useState<boolean>(true);
-  return (
-    <ScrollAreaPrimitive.Root
-      ref={ref}
-      onScrollCapture={() => setHidden((previous) => !previous)}
-      className={cn("relative overflow-hidden", className)}
-      {...props}
-    >
-      <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
-        {children}
-      </ScrollAreaPrimitive.Viewport>
-      <ScrollBar hidden={hidden} />
-      <ScrollAreaPrimitive.Corner />
-    </ScrollAreaPrimitive.Root>
-  );
-});
-ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName;
-
-const ScrollBar = React.forwardRef<
-  React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
->(({ hidden, className, orientation = "vertical", ...props }, ref) => {
   // If the scrollbar is not hidden, it remains visible for 500ms before fading out
-  // `hidden` is a prop which trigger the useEffect each time the scrollbar is used
-  const [visible, setVisible] = React.useState<boolean>(false);
+  const [opacity, setOpacity] = React.useState<number>(0);
   const timeout = React.useRef<NodeJS.Timeout | null>(null);
 
   // Set visible each time the scrollbar is used
-  React.useEffect(() => {
+  const onScrollCapture = React.useCallback(() => {
     if (timeout.current) {
       clearTimeout(timeout.current);
     }
-    setVisible(true);
+    setOpacity(100);
     timeout.current = setTimeout(() => {
-      setVisible(false);
+      setOpacity(0);
     }, 500);
-  }, [hidden]);
+  }, []);
 
   // Cleanup the timeout if the component unmounts
   React.useEffect(() => {
@@ -57,24 +34,42 @@ const ScrollBar = React.forwardRef<
   }, []);
 
   return (
-    <ScrollAreaPrimitive.ScrollAreaScrollbar
+    <ScrollAreaPrimitive.Root
       ref={ref}
-      orientation={orientation}
-      className={cn(
-        "touch-none select-none transition-opacity duration-300",
-        orientation === "vertical" &&
-          "h-full w-2.5 border-l border-l-transparent p-[1px]",
-        orientation === "horizontal" &&
-          "h-2.5 flex-col border-t border-t-transparent p-[1px]",
-        visible ? "opacity-100" : "opacity-0",
-        className,
-      )}
+      onScrollCapture={onScrollCapture}
+      className={cn("relative overflow-hidden", className)}
       {...props}
     >
-      <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
-    </ScrollAreaPrimitive.ScrollAreaScrollbar>
+      <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
+        {children}
+      </ScrollAreaPrimitive.Viewport>
+      <ScrollBar className={`opacity-${opacity}`} />
+      <ScrollAreaPrimitive.Corner />
+    </ScrollAreaPrimitive.Root>
   );
 });
+ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName;
+
+const ScrollBar = React.forwardRef<
+  React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
+>(({ className, orientation = "vertical", ...props }, ref) => (
+  <ScrollAreaPrimitive.ScrollAreaScrollbar
+    ref={ref}
+    orientation={orientation}
+    className={cn(
+      "touch-none select-none transition-opacity duration-300",
+      orientation === "vertical" &&
+        "h-full w-2.5 border-l border-l-transparent p-[1px]",
+      orientation === "horizontal" &&
+        "h-2.5 flex-col border-t border-t-transparent p-[1px]",
+      className,
+    )}
+    {...props}
+  >
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+  </ScrollAreaPrimitive.ScrollAreaScrollbar>
+));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
 
 export { ScrollArea, ScrollBar };
