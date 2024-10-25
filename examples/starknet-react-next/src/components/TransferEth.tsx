df5ff27c37fce90c600579179244c1e86b0ea3d7
broody <broody@gmail.com>
2024-10-24 15:31:46 -1000

    recursive error

--- examples/starknet-react-next/src/components/TransferEth.tsx
+++ examples/starknet-react-next/src/components/TransferEth.tsx
@@ -21,8 +21,22 @@ export const TransferEth = () => {
       //setSubmitted(true);
       setTxnHash(undefined);
 
-      await account.estimateInvokeFee(
-[
+//       await account.estimateInvokeFee(
+// [
+//           {
+//             contractAddress: ETH_CONTRACT,
+//             entrypoint: "approve",
+//             calldata: [account?.address, amount, "0x0"],
+//           },
+//           {
+//             contractAddress: ETH_CONTRACT,
+//             entrypoint: "transfer",
+//             calldata: [account?.address, amount, "0x0"],
+//           },
+//         ]);
+
+      account
+        .execute([
           {
             contractAddress: ETH_CONTRACT,
             entrypoint: "approve",
@@ -34,23 +48,9 @@ export const TransferEth = () => {
             calldata: [account?.address, amount, "0x0"],
           },
         ]);
-
-      // account
-      //   .execute([
-      //     {
-      //       contractAddress: ETH_CONTRACT,
-      //       entrypoint: "approve",
-      //       calldata: [account?.address, amount, "0x0"],
-      //     },
-      //     {
-      //       contractAddress: ETH_CONTRACT,
-      //       entrypoint: "transfer",
-      //       calldata: [account?.address, amount, "0x0"],
-      //     },
-      //   ])
-      //   .then(({ transaction_hash }) => setTxnHash(transaction_hash))
-      //   .catch((e) => console.error(e))
-      //   .finally(() => setSubmitted(false));
+        // .then(({ transaction_hash }) => setTxnHash(transaction_hash))
+        // .catch((e) => console.error(e))
+        // .finally(() => setSubmitted(false));
     },
     [account],
   );
