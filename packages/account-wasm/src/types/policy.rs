3db96b61bb6812412ff426564e70b60278cd4262
Tarrence van As <tarrence@vanas.family>
2024-10-30 13:23:22 -0400

    Make policy backward compat

--- packages/account-wasm/src/types/policy.rs
+++ packages/account-wasm/src/types/policy.rs
@@ -13,24 +13,10 @@ use super::EncodingError;
 #[allow(non_snake_case)]
 #[derive(Tsify, Serialize, Deserialize, Debug, Clone)]
 #[tsify(into_wasm_abi, from_wasm_abi)]
-pub struct Policy {
-    pub call_policy: Option<CallPolicy>,
-    pub typed_data_policy: Option<TypedDataPolicy>,
-}
-
-#[allow(non_snake_case)]
-#[derive(Tsify, Serialize, Deserialize, Debug, Clone)]
-#[tsify(into_wasm_abi, from_wasm_abi)]
-pub struct CallPolicy {
-    pub target: String,
-    pub method: String,
-}
-
-#[allow(non_snake_case)]
-#[derive(Tsify, Serialize, Deserialize, Debug, Clone)]
-#[tsify(into_wasm_abi, from_wasm_abi)]
-pub struct TypedDataPolicy {
-    pub type_hash: String,
+#[serde(untagged)]
+pub enum Policy {
+    Call { target: String, method: String },
+    TypedData { type_hash: String },
 }
 
 impl TryFrom<JsValue> for Policy {
@@ -45,39 +31,27 @@ impl TryFrom<Policy> for SdkPolicy {
     type Error = EncodingError;
 
     fn try_from(value: Policy) -> Result<Self, Self::Error> {
-        if value.call_policy.is_some() && value.typed_data_policy.is_some() {
-            return Err(EncodingError::UnexpectedOption("Policy: Both call_policy and typed_data_policy are Some when exactly one is expected".to_string()));
+        match value {
+            Policy::Call { target, method } => Ok(SdkPolicy::Call(SdkCallPolicy {
+                contract_address: Felt::from_str(&target)?,
+                selector: get_selector_from_name(&method).unwrap(),
+            })),
+            Policy::TypedData { type_hash } => Ok(SdkPolicy::TypedData(SdkTypedDataPolicy {
+                type_hash: Felt::from_str(&type_hash)?,
+            })),
         }
-        if let Some(call_policy) = value.call_policy {
-            return Ok(SdkPolicy::Call(SdkCallPolicy {
-                contract_address: Felt::from_str(&call_policy.target)?,
-                selector: get_selector_from_name(&call_policy.method).unwrap(),
-            }));
-        }
-        if let Some(typed_data_policy) = value.typed_data_policy {
-            return Ok(SdkPolicy::TypedData(SdkTypedDataPolicy {
-                type_hash: Felt::from_str(&typed_data_policy.type_hash)?,
-            }));
-        }
-        Err(EncodingError::UnexpectedOption("Policy: Neither call_policy nor typed_data_policy is Some when exactly one is expected".to_string()))
     }
 }
 
 impl From<SdkPolicy> for Policy {
     fn from(value: SdkPolicy) -> Self {
         match value {
-            SdkPolicy::Call(call_policy) => Self {
-                call_policy: Some(CallPolicy {
-                    target: call_policy.contract_address.to_string(),
-                    method: call_policy.selector.to_string(),
-                }),
-                typed_data_policy: None,
+            SdkPolicy::Call(call_policy) => Policy::Call {
+                target: call_policy.contract_address.to_string(),
+                method: call_policy.selector.to_string(),
             },
-            SdkPolicy::TypedData(typed_data_policy) => Self {
-                call_policy: None,
-                typed_data_policy: Some(TypedDataPolicy {
-                    type_hash: typed_data_policy.type_hash.to_string(),
-                }),
+            SdkPolicy::TypedData(typed_data_policy) => Policy::TypedData {
+                type_hash: typed_data_policy.type_hash.to_string(),
             },
         }
     }
