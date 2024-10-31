aea518278149f454b8eacea1bf8f2b452e70a5aa
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-11-01 06:39:39 +1100

    og image

--- documentation/vocs.config.ts
+++ documentation/vocs.config.ts
@@ -1,130 +1,130 @@
 import { defineConfig } from "vocs";
 
 export default defineConfig({
-  title: "Cartridge Controller",
-  description: "Cartridge | Tools for onchain development",
-  iconUrl: "/Cartridge.svg",
-  logoUrl: "/Cartridge.svg",
-  ogImageUrl:
-    "https://og-image.preview.cartridge.gg/api/og?logo=%https://www.dojoengine.org/dojo-icon.svg&title=%title&description=%description",
+    title: "Cartridge Controller",
+    description: "Cartridge | Tools for onchain development",
+    iconUrl: "/Cartridge.svg",
+    logoUrl: "/Cartridge.svg",
+    ogImageUrl:
+        "https://og-image.preview.cartridge.gg/api/cartridge?logo=%https://www.dojoengine.org/dojo-icon.svg&title=%title&description=%description",
 
-  theme: {
-    colorScheme: "dark",
-    variables: {
-      color: {
-        textAccent: "#ffc52a",
-        background: "#0c0c0c",
-        backgroundDark: "#121212",
-        noteBackground: "#1a1a1a",
-      },
+    theme: {
+        colorScheme: "dark",
+        variables: {
+            color: {
+                textAccent: "#ffc52a",
+                background: "#0c0c0c",
+                backgroundDark: "#121212",
+                noteBackground: "#1a1a1a",
+            },
+        },
     },
-  },
-  font: {
-    google: "Open Sans",
-  },
-  topNav: [
-    {
-      text: "v0.5.0-alpha.4",
-      items: [
+    font: {
+        google: "Open Sans",
+    },
+    topNav: [
         {
-          text: "Releases",
-          link: "https://github.com/cartridge-gg/controller/releases",
+            text: "v0.5.0-alpha.4",
+            items: [
+                {
+                    text: "Releases",
+                    link: "https://github.com/cartridge-gg/controller/releases",
+                },
+                {
+                    text: "Changelog",
+                    link: "https://github.com/cartridge-gg/controller/releases",
+                },
+            ],
+        },
+    ],
+    socials: [
+        {
+            icon: "github",
+            link: "https://github.com/cartridge-gg/controller",
         },
         {
-          text: "Changelog",
-          link: "https://github.com/cartridge-gg/controller/releases",
+            icon: "x",
+            link: "https://x.com/cartridge_gg",
         },
-      ],
+    ],
+    editLink: {
+        pattern:
+            "https://github.com/cartridge-gg/controller/blob/main/docs/pages/:path",
+        text: "Edit on GitHub",
     },
-  ],
-  socials: [
-    {
-      icon: "github",
-      link: "https://github.com/cartridge-gg/controller",
-    },
-    {
-      icon: "x",
-      link: "https://x.com/cartridge_gg",
-    },
-  ],
-  editLink: {
-    pattern:
-      "https://github.com/cartridge-gg/controller/blob/main/docs/pages/:path",
-    text: "Edit on GitHub",
-  },
 
-  // Banner configuration
-  banner: {
-    dismissable: false,
-    backgroundColor: "red",
-    content: "Join us in [Discord](https://discord.gg/cartridge)!",
-    height: "28px",
-    textColor: "white",
-  },
-  sidebar: [
-    {
-      text: "Controller",
-      items: [
-        {
-          text: "Overview",
-          link: "/controller/overview",
-        },
-        {
-          text: "Getting Started",
-          link: "/controller/getting-started",
-        },
-        {
-          text: "Sessions",
-          link: "/controller/sessions",
-        },
-        {
-          text: "Configuration",
-          link: "/controller/configuration",
-        },
-        {
-          text: "Theming",
-          link: "/controller/theming",
-        },
-        {
-          text: "Controller Examples",
-          items: [
-            {
-              text: "React",
-              link: "/controller/examples/react",
-            },
-            {
-              text: "Svelte",
-              link: "/controller/examples/svelte",
-            },
-            {
-              text: "Rust",
-              link: "/controller/examples/rust",
-            },
-            {
-              text: "Telegram",
-              link: "/controller/examples/telegram",
-            },
-          ],
-        },
-      ],
+    // Banner configuration
+    banner: {
+        dismissable: false,
+        backgroundColor: "red",
+        content: "Join us in [Discord](https://discord.gg/cartridge)!",
+        height: "28px",
+        textColor: "white",
     },
-    {
-      text: "Slot",
-      items: [
+    sidebar: [
         {
-          text: "Getting Started",
-          link: "/slot/getting-started",
+            text: "Controller",
+            items: [
+                {
+                    text: "Overview",
+                    link: "/controller/overview",
+                },
+                {
+                    text: "Getting Started",
+                    link: "/controller/getting-started",
+                },
+                {
+                    text: "Sessions",
+                    link: "/controller/sessions",
+                },
+                {
+                    text: "Configuration",
+                    link: "/controller/configuration",
+                },
+                {
+                    text: "Theming",
+                    link: "/controller/theming",
+                },
+                {
+                    text: "Controller Examples",
+                    items: [
+                        {
+                            text: "React",
+                            link: "/controller/examples/react",
+                        },
+                        {
+                            text: "Svelte",
+                            link: "/controller/examples/svelte",
+                        },
+                        {
+                            text: "Rust",
+                            link: "/controller/examples/rust",
+                        },
+                        {
+                            text: "Telegram",
+                            link: "/controller/examples/telegram",
+                        },
+                    ],
+                },
+            ],
         },
-      ],
-    },
-    {
-      text: "VRF",
-      items: [
         {
-          text: "Overview",
-          link: "/vrf/overview",
+            text: "Slot",
+            items: [
+                {
+                    text: "Getting Started",
+                    link: "/slot/getting-started",
+                },
+            ],
         },
-      ],
-    },
-  ],
+        {
+            text: "VRF",
+            items: [
+                {
+                    text: "Overview",
+                    link: "/vrf/overview",
+                },
+            ],
+        },
+    ],
 });
