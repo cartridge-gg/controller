5e1f0a76f54d98d6b09abcd382f1cf0b1a631c7a
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-11-01 09:25:38 +1100

    clean up tailwind

--- documentation/tailwind.config.cjs
+++ documentation/tailwind.config.cjs
@@ -6,15 +6,6 @@ module.exports = {
       colors: {
         primary: "#ffc52a", // Custom primary color
       },
-      keyframes: {
-        "gradient-x": {
-          "0%, 100%": { "background-position": "0% 50%" },
-          "50%": { "background-position": "100% 50%" },
-        },
-      },
-      animation: {
-        "gradient-x": "gradient-x 5s linear infinite",
-      },
     },
   },
   plugins: [],
