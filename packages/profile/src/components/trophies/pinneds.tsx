fb84fcdaec329af5eaa43efaad7708917f0d681b
bal7hazar <bal7hazar@proton.me>
2024-11-06 16:17:09 +0100

    💄 Code style

--- packages/profile/src/components/trophies/pinneds.tsx
+++ packages/profile/src/components/trophies/pinneds.tsx
@@ -5,7 +5,11 @@ export function Pinneds({ achievements }: { achievements: Item[] }) {
   return (
     <div className="grid grid-cols-3 gap-4">
       {achievements.map((achievement) => (
-        <Pinned key={achievement.id} icon={achievement.icon} title={achievement.title} />
+        <Pinned
+          key={achievement.id}
+          icon={achievement.icon}
+          title={achievement.title}
+        />
       ))}
       {Array.from({ length: 3 - achievements.length }).map((_, index) => (
         <Empty key={index} />
