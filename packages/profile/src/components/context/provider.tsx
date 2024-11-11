2cb7ebdd09e9d1c0a71b690a7583a0de7531f8ef
Tarrence van As <tarrence@vanas.family>
2024-11-11 12:08:26 -0500

    Enable posthog for profile

--- packages/profile/src/components/context/provider.tsx
+++ packages/profile/src/components/context/provider.tsx
@@ -6,25 +6,39 @@ import { BrowserRouter } from "react-router-dom";
 import { CartridgeAPIProvider } from "@cartridge/utils/api/cartridge";
 import { IndexerAPIProvider } from "@cartridge/utils/api/indexer";
 import { DataProvider } from "./data";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "always",
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+  });
+}
 
 export function Provider({ children }: PropsWithChildren) {
   const queryClient = new QueryClient();
 
   return (
-    <BrowserRouter>
-      <ThemeProvider defaultScheme="system">
-        <CartridgeAPIProvider
-          url={`${import.meta.env.VITE_CARTRIDGE_API_URL!}/query`}
-        >
-          <IndexerAPIProvider credentials="omit">
-            <QueryClientProvider client={queryClient}>
-              <ConnectionProvider>
-                <DataProvider>{children}</DataProvider>
-              </ConnectionProvider>
-            </QueryClientProvider>
-          </IndexerAPIProvider>
-        </CartridgeAPIProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <PostHogProvider client={posthog}>
+      <BrowserRouter>
+        <ThemeProvider defaultScheme="system">
+          <CartridgeAPIProvider
+            url={`${import.meta.env.VITE_CARTRIDGE_API_URL!}/query`}
+          >
+            <IndexerAPIProvider credentials="omit">
+              <QueryClientProvider client={queryClient}>
+                <ConnectionProvider>
+                  <DataProvider>{children}</DataProvider>
+                </ConnectionProvider>
+              </QueryClientProvider>
+            </IndexerAPIProvider>
+          </CartridgeAPIProvider>
+        </ThemeProvider>
+      </BrowserRouter>
+    </PostHogProvider>
   );
 }
