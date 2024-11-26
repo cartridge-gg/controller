a1ce512e0c9c1df24ce1258d0383b05ad2749dcc
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-11-26 16:43:41 +0100

    Fix re-render when opening modal

--- packages/profile/src/components/account.tsx
+++ packages/profile/src/components/account.tsx
@@ -2,11 +2,16 @@ import { Navigate, Outlet, useLocation, useParams } from "react-router-dom";
 
 export function Account() {
   const location = useLocation();
-  const { username } = useParams<{
+  const { username, project } = useParams<{
     username: string;
+    project: string;
   }>();
 
-  if (location.pathname === `/account/${username}`) {
+  if (
+    [`/account/${username}`, `/account/${username}/slot/${project}`].includes(
+      location.pathname,
+    )
+  ) {
     return <Navigate to="inventory" replace />;
   }
 
