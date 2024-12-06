b13959df42e7dc5256d974e7855bce681996a077
Yun <broody@gmail.com>
2024-12-06 04:02:16 -1000

    Add lookup methods to controller pkg (#1106)
    * Add lookup methods to controller pkg
    
    * fix warning
    
    * use workspace

--- packages/controller/src/index.ts
+++ packages/controller/src/index.ts
@@ -1,5 +1,7 @@
 export { default } from "./controller";
 export * from "./errors";
 export * from "./types";
+export * from "./lookup";
+
 export { toWasmPolicies, toSessionPolicies, toArray } from "./utils";
 export * from "@cartridge/presets";
