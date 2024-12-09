c7c187aec2cd55610cff1ed8ff22677b92a90c96
Tarrence van As <tarrence@vanas.family>
2024-12-09 16:34:36 -0500

    Fix Starknet Config

--- packages/keychain/src/components/Provider/index.tsx
+++ packages/keychain/src/components/Provider/index.tsx
@@ -8,8 +8,9 @@ import posthog from "posthog-js";
 import { ConnectionContext } from "./connection";
 import { ControllerThemeProvider } from "./theme";
 import { jsonRpcProvider, StarknetConfig, voyager } from "@starknet-react/core";
-import { sepolia, mainnet } from "@starknet-react/chains";
+import { sepolia, mainnet, Address } from "@starknet-react/chains";
 import { constants, num } from "starknet";
+import { STRK_CONTRACT_ADDRESS } from "@cartridge/utils";
 
 export function Provider({ children }: PropsWithChildren) {
   const connection = useConnectionValue();
@@ -25,12 +26,58 @@ export function Provider({ children }: PropsWithChildren) {
       default:
         nodeUrl = connection.rpcUrl;
     }
+    console.log(nodeUrl);
     return { nodeUrl };
   }, [connection.rpcUrl, connection.chainId]);
 
-  const defaultChainId = useMemo(() => {
-    return num.toBigInt(connection.chainId || 0);
-  }, [connection.chainId]);
+  const config = useCallback(() => {
+    switch (connection.chainId) {
+      case constants.StarknetChainId.SN_MAIN:
+        return {
+          explorer: voyager,
+          chains: [mainnet],
+          provider: jsonRpcProvider({ rpc }),
+        };
+      case constants.StarknetChainId.SN_SEPOLIA:
+        return {
+          explorer: voyager,
+          chains: [sepolia],
+          provider: jsonRpcProvider({ rpc }),
+        };
+      default:
+        return {
+          explorer: undefined,
+          chains: [
+            {
+              id: num.toBigInt(connection.chainId || 0),
+              network: "slot",
+              name: "Slot",
+              nativeCurrency: {
+                address: STRK_CONTRACT_ADDRESS as Address,
+                name: "Stark",
+                symbol: "STRK",
+                decimals: 18,
+              },
+              testnet: true,
+              rpcUrls: {
+                default: {
+                  http: [connection.rpcUrl || ""],
+                },
+                public: {
+                  http: [connection.rpcUrl || ""],
+                },
+              },
+              explorers: {
+                worldexplorer: [""],
+              },
+            },
+          ],
+          provider: jsonRpcProvider({ rpc }),
+        };
+    }
+  }, [connection.chainId, rpc]);
+
+  const { explorer, chains, provider } = config();
 
   return (
     <CartridgeAPIProvider url={ENDPOINT}>
@@ -38,10 +85,9 @@ export function Provider({ children }: PropsWithChildren) {
         <ConnectionContext.Provider value={connection}>
           <ControllerThemeProvider>
             <StarknetConfig
-              explorer={voyager}
-              chains={[sepolia, mainnet]}
-              defaultChainId={defaultChainId}
-              provider={jsonRpcProvider({ rpc })}
+              explorer={explorer}
+              chains={chains}
+              provider={provider}
             >
               <PostHogProvider client={posthog}>{children}</PostHogProvider>
             </StarknetConfig>
