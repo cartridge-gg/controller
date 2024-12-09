8462cd3437cc8bde5f31ff03efcd38e09bfdbc46
Tarrence van As <tarrence@vanas.family>
2024-12-08 20:32:40 -0500

    Rewrite authenticate url to remove query params

--- packages/keychain/src/pages/authenticate.tsx
+++ packages/keychain/src/pages/authenticate.tsx
@@ -2,21 +2,43 @@ import { useRouter } from "next/router";
 import { AuthAction, Authenticate as AuthComponent } from "components/connect";
 import dynamic from "next/dynamic";
 import { constants } from "starknet";
+import { useEffect, useState } from "react";
 
 // auth page used for externally embedded keychain
 function Authenticate() {
   const router = useRouter();
-  const { name, action, network } = router.query as {
+  const [params, setParams] = useState<{
     name: string;
     action: string;
     network?: string;
-  };
+  }>();
+
+  useEffect(() => {
+    if (router.isReady && !params) {
+      const { name, action, network } = router.query as {
+        name: string;
+        action: string;
+        network?: string;
+      };
+
+      if (name && action) {
+        setParams({ name, action, network });
+
+        // Remove query params to avoid issues with password managers
+        router.replace(router.pathname, undefined, { shallow: true });
+      }
+    }
+  }, [router.isReady, params]);
+
+  if (!params) {
+    return null;
+  }
 
   return (
     <AuthComponent
-      name={decodeURIComponent(name)}
-      action={decodeURIComponent(action) as AuthAction}
-      network={network ?? constants.NetworkName.SN_MAIN}
+      name={decodeURIComponent(params.name)}
+      action={decodeURIComponent(params.action) as AuthAction}
+      network={params.network ?? constants.NetworkName.SN_MAIN}
       onSuccess={() => {
         if (window.opener) {
           return window.close();
