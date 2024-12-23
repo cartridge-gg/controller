63855771cc574b5c3f06a08ec398e2ad28b4183c
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-12-23 10:20:44 +0900

    Switch ui-next toast

--- packages/keychain/src/components/Funding/DepositEth.tsx
+++ packages/keychain/src/components/Funding/DepositEth.tsx
@@ -28,13 +28,13 @@ import {
   CopyAddress,
 } from "@cartridge/ui-next";
 import { useConnection } from "@/hooks/connection";
-import { useToast } from "@/hooks/toast";
 import { ErrorAlert } from "../ErrorAlert";
 import { parseEther } from "viem";
 import { AmountSelection, DEFAULT_AMOUNT } from "./AmountSelection";
 import { Balance } from "./Balance";
 import { TokenPair, usePriceQuery } from "@cartridge/utils/api/cartridge";
 import { ETH_CONTRACT_ADDRESS } from "@cartridge/utils";
+import { toast } from "sonner";
 
 type DepositEthProps = {
   onComplete?: (deployHash?: string) => void;
@@ -53,7 +53,6 @@ function DepositEthInner({ onComplete, onBack }: DepositEthProps) {
   const { connectAsync, connectors, isPending: isConnecting } = useConnect();
   const { controller, chainId } = useConnection();
   const { account: extAccount } = useAccount();
-  const { toast } = useToast();
 
   const [dollarAmount, setDollarAmount] = useState<number>(DEFAULT_AMOUNT);
   const [state, setState] = useState<"connect" | "fund">("connect");
@@ -144,8 +143,8 @@ function DepositEthInner({ onComplete, onBack }: DepositEthProps) {
     if (!controller?.address) return;
 
     navigator.clipboard.writeText(controller.address);
-    toast("Copied");
-  }, [controller?.address, toast]);
+    toast.success("Address copied");
+  }, [controller?.address]);
 
   return (
     <Container
