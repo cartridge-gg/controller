91a2d36dff22c18c1834e8c844b84dfcc5d786c2
Tarrence van As <tarrencev@users.noreply.github.com>
2025-01-03 12:44:41 -0500

    Create Controller ui tweaks (#1232)
    * Create Controller ui tweaks
    
    * Update snapshots

--- scripts/update-storybook-snapshot.sh
+++ scripts/update-storybook-snapshot.sh
@@ -5,8 +5,8 @@ docker run \
   --rm \
   -v "$(pwd)":/app \
   -v /app/node_modules \
-  -it \
-  ghcr.io/cartridge-gg/controller/storybook-env:sha-b6d96629787539de6f5e464aafedafd4eb3ffcc7 \
+  --ipc=host \
+  storybook-env \
   bash -c "
     pnpm install --frozen-lockfile && \
     pnpm test:storybook:update
