d0b0ed29dcf61cc2fd9d0f6ae920c4ed53bc08ed
JunichiSugiura <jun.sugiura.jp@gmail.com>
2025-01-07 11:17:10 +0900

    Observe dark mode class changes

--- packages/ui-next/src/hooks/theme.ts
+++ packages/ui-next/src/hooks/theme.ts
@@ -11,45 +11,66 @@ export function useThemeEffect({
   useEffect(() => {
     if (!theme) return;
 
-    const appliedColorMode = document.documentElement.className.includes("dark")
-      ? "dark"
-      : "light";
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        const appliedColorMode = (
+          mutation.target as HTMLElement
+        ).classList.contains("dark")
+          ? "dark"
+          : "light";
 
-    document.documentElement.style.setProperty(
-      "--theme-icon-url",
-      theme.icon.startsWith("http")
-        ? `url("${theme.icon}")`
-        : `url("${assetUrl}${theme.icon}")`,
-    );
-    const coverValue =
-      typeof theme.cover === "string"
-        ? theme.cover
-        : theme.cover[appliedColorMode];
-    const coverUrl = coverValue.startsWith("http")
-      ? `url("${coverValue}")`
-      : `url("${assetUrl}${coverValue}")`;
-    document.documentElement.style.setProperty("--theme-cover-url", coverUrl);
+        document.documentElement.style.setProperty(
+          "--theme-icon-url",
+          theme.icon.startsWith("http")
+            ? `url("${theme.icon}")`
+            : `url("${assetUrl}${theme.icon}")`,
+        );
+        const coverValue =
+          typeof theme.cover === "string"
+            ? theme.cover
+            : theme.cover[appliedColorMode];
+        const coverUrl = coverValue.startsWith("http")
+          ? `url("${coverValue}")`
+          : `url("${assetUrl}${coverValue}")`;
+        document.documentElement.style.setProperty(
+          "--theme-cover-url",
+          coverUrl,
+        );
 
-    if (!theme.colors) return;
+        if (!theme.colors) return;
 
-    if (theme.colors?.primary) {
-      const val =
-        typeof theme.colors.primary === "string"
-          ? theme.colors?.primary
-          : theme.colors?.primary[appliedColorMode];
-      document.documentElement.style.setProperty("--primary", hexToHsl(val));
-    }
+        if (theme.colors?.primary) {
+          const val =
+            typeof theme.colors.primary === "string"
+              ? theme.colors?.primary
+              : theme.colors?.primary[appliedColorMode];
+          document.documentElement.style.setProperty(
+            "--primary",
+            hexToHsl(val),
+          );
+        }
 
-    if (theme.colors?.primaryForeground) {
-      const val =
-        typeof theme.colors.primaryForeground === "string"
-          ? theme.colors?.primaryForeground
-          : theme.colors?.primaryForeground[appliedColorMode];
-      document.documentElement.style.setProperty(
-        "--primary-foreground",
-        hexToHsl(val),
-      );
-    }
+        if (theme.colors?.primaryForeground) {
+          const val =
+            typeof theme.colors.primaryForeground === "string"
+              ? theme.colors?.primaryForeground
+              : theme.colors?.primaryForeground[appliedColorMode];
+          document.documentElement.style.setProperty(
+            "--primary-foreground",
+            hexToHsl(val),
+          );
+        }
+      });
+    });
+
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["class"],
+    });
+
+    return () => {
+      observer.disconnect();
+    };
   }, [theme, assetUrl]);
 }
 
