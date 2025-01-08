b4fc7df5d799beb26a6b90a6703623a32657d988
bal7hazar <bal7hazar@proton.me>
2025-01-08 18:52:23 +0100

    🔧 Turn example into https and profile handles wasm cfg

--- packages/profile/vite.config.ts
+++ packages/profile/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
+import wasm from "vite-plugin-wasm";
 import react from "@vitejs/plugin-react-swc";
 import process from "node:process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), wasm()],
   server: {
     port: process.env.NODE_ENV === "development" ? 3003 : undefined,
   },
