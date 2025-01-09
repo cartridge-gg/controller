60699205e48181b042d8d0ae28a5262e86d53741
broody <broody@gmail.com>
2025-01-08 21:59:24 -1000

    update

--- examples/next/src/components/providers/StarknetProvider.tsx
+++ examples/next/src/components/providers/StarknetProvider.tsx
@@ -121,7 +121,7 @@ export function StarknetProvider({ children }: PropsWithChildren) {
 
 const controller = new ControllerConnector({
   policies,
-  rpcUrl: rpc,
+  rpc,
   url:
     process.env.NEXT_PUBLIC_KEYCHAIN_DEPLOYMENT_URL ??
     process.env.NEXT_PUBLIC_KEYCHAIN_FRAME_URL,
