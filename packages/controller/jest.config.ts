29c8c6f9dbad390968fb845c4305d073480c76d7
Yun <broody@gmail.com>
2025-01-15 05:35:42 -1000

    Encode slot chain id and add testing (#1275)

---
+++ packages/controller/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+};
+
+export default config;
