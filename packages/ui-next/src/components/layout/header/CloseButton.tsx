0b17d4f4660c0b4e83e9af6e283616fc2f8046fe
JunichiSugiura <jun.sugiura.jp@gmail.com>
2025-01-22 17:17:08 +0900

    Share keychain layout component with profile via ui package

---
+++ packages/ui-next/src/components/layout/header/CloseButton.tsx
@@ -0,0 +1,35 @@
+import { TimesIcon, Button } from "@cartridge/ui-next";
+import { isIframe } from "@cartridge/utils";
+// import { useConnection } from "@/hooks/connection";
+import { useCallback } from "react";
+
+export function CloseButton({ onClose }: { onClose?: () => void }) {
+  // const { upgrade, logout, closeModal } = useConnection();
+
+  const handleClose = useCallback(() => {
+    // if (upgrade.available) {
+    //   logout();
+    // }
+
+    if (onClose) {
+      onClose();
+    }
+
+    // closeModal();
+  }, [
+    // upgrade.available,
+    // logout,
+    // closeModal,
+    onClose,
+  ]);
+
+  if (!isIframe()) {
+    return null;
+  }
+
+  return (
+    <Button variant="icon" size="icon" onClick={handleClose}>
+      <TimesIcon />
+    </Button>
+  );
+}
---
+++ packages/ui-next/src/components/layout/header/TopBar.tsx
@@ -0,0 +1,57 @@
+import { CloseButton } from "./CloseButton";
+// import { SettingsButton } from "./SettingsButton";
+import { BackButton } from "./BackButton";
+// import { useConnection } from "@/hooks/connection";
+// import { useMemo } from "react";
+// import { Network } from "@cartridge/ui-next";
+
+export type TopBarProps = {
+  onBack?: () => void;
+  hideAccount?: boolean;
+  hideNetwork?: boolean;
+  onClose?: () => void;
+};
+
+export function TopBar({
+  onBack,
+  hideAccount,
+  // hideNetwork,
+  onClose,
+}: TopBarProps) {
+  // const { openSettings, context, chainId } = useConnection();
+  // const showSettings = useMemo(
+  //   () => !context || !["connect", "open-settings"].includes(context.type),
+  //   [
+  //     context
+  //   ],
+  // );
+
+  return (
+    <div className="flex items-center justify-between absolute top-0 left-0 right-0 h-14 p-2 z-50">
+      <div>
+        {onBack ? (
+          <BackButton onClick={onBack} />
+        ) : (
+          <CloseButton onClose={onClose} />
+        )}
+      </div>
+
+      <div className="flex items-center gap-2">
+        {/* {!hideNetwork && chainId && <Network chainId={chainId} />} */}
+
+        {!hideAccount && (
+          <>
+            {/* {!!address && (
+            <>
+              <EthBalance chainId={chainId} address={address} />
+              {chainId && <AccountMenu onLogout={onLogout} address={address} />}
+            </>
+          )} */}
+          </>
+        )}
+
+        {/* {showSettings && <SettingsButton onClick={() => openSettings()} />} */}
+      </div>
+    </div>
+  );
+}
