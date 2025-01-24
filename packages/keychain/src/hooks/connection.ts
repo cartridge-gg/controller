158ebe6752100b3a1725457b267a2525e3d1818f
Tarrence van As <tarrence@vanas.family>
2025-01-23 21:06:05 -0500

    Set policies through url params

--- packages/keychain/src/hooks/connection.ts
+++ packages/keychain/src/hooks/connection.ts
@@ -209,7 +209,6 @@ export function useConnectionValue() {
     const connection = connectToController<ParentMethods>({
       setOrigin,
       setRpcUrl,
-      setPolicies,
       setContext,
       setController,
     });
