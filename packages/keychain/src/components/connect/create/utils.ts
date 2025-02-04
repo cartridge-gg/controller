ca6150a38c3fa83c36b4cb3a36d2875f4e7fee16
nrmnqdds <quddusnuriman@gmail.com>
2025-02-04 16:40:17 +0800

    creates helper function to disable policy toggle

--- packages/keychain/src/components/connect/create/utils.ts
+++ packages/keychain/src/components/connect/create/utils.ts
@@ -1,9 +1,9 @@
+import { fetchData } from "@/utils/graphql";
 import {
   AccountDocument,
-  AccountQuery,
-  AccountQueryVariables,
+  type AccountQuery,
+  type AccountQueryVariables,
 } from "@cartridge/utils/api/cartridge";
-import { fetchData } from "@/utils/graphql";
 
 export function fetchAccount(username: string, signal?: AbortSignal) {
   return fetchData<AccountQuery, AccountQueryVariables>(
@@ -14,3 +14,30 @@ export function fetchAccount(username: string, signal?: AbortSignal) {
     signal,
   );
 }
+
+/**
+ * Checks if the policy is required.
+ * @param requiredPolicyTypes The required policy types.
+ * @param type The policy type to check.
+ * @returns True if the policy is required.
+ */
+export const isPolicyRequired = ({
+  requiredPolicyTypes,
+  type,
+}: {
+  /**
+   * The required policy types.
+   * @example ["policyType1", "policyType2"]
+   */
+  requiredPolicyTypes: Array<string>;
+  /**
+   * The policy type to check.
+   */
+  type: Nullable<string>;
+}): boolean => {
+  if (!type) return false;
+
+  return requiredPolicyTypes.includes(type);
+};
+
+type Nullable<T> = T | null | undefined;
