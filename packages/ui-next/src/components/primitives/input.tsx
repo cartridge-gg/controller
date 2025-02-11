28bb47dd348a572312f5edff19ecf220bd6d8507
bal7hazar <bal7hazar@proton.me>
2025-02-11 20:15:54 +0100

    ✨ Add size management inside input

--- packages/ui-next/src/components/primitives/input.tsx
+++ packages/ui-next/src/components/primitives/input.tsx
@@ -2,23 +2,42 @@ import * as React from "react";
 
 import { cn } from "@/utils";
 import { Clear } from "./clear";
+import { cva, VariantProps } from "class-variance-authority";
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+export interface InputProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size">,
+    VariantProps<typeof inputVariants> {
   isLoading?: boolean;
   onClear?: () => void;
-};
+}
+
+export const inputVariants = cva(
+  "flex w-full rounded-md border px-4 py-3.5 font-mono ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-foreground-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "border-input bg-background-200",
+      },
+      size: {
+        default: "h-10 text-sm/[18px]",
+        lg: "h-12 text-[15px]/5",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+);
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ isLoading, onClear, className, type, ...props }, ref) => {
+  ({ isLoading, onClear, variant, size, className, type, ...props }, ref) => {
     return (
       <div className="relative">
         <input
-          type={type}
-          className={cn(
-            "flex h-10 w-full rounded-md border border-input bg-background-200 px-4 py-3.5 font-mono text-md ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-foreground-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
-            className,
-          )}
           ref={ref}
+          type={type}
+          className={cn(inputVariants({ variant, size, className }))}
           {...props}
         />
         {!!props.value && !!onClear && (
