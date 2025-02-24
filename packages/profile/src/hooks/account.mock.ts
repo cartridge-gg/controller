1a5fd95da04d973c2c710df7c4636a12cf0d8190
JunichiSugiura <jun.sugiura.jp@gmail.com>
2025-02-24 18:21:26 +0900

    Manually declare mock function type

--- packages/profile/src/hooks/account.mock.ts
+++ packages/profile/src/hooks/account.mock.ts
@@ -4,16 +4,16 @@ import { UseAccountInfoResponse, UseAccountResponse } from "#hooks/account";
 
 export * from "./account";
 
-export const useAccount = fn(() => ({
+export const useAccount: Mock<() => UseAccountResponse> = fn(() => ({
   username: accounts["test-0"].username,
   address: accounts["test-0"].address,
-})).mockName("useAccount") as Mock<() => UseAccountResponse>; // TS doesn't infer the return type of the mock for some reason
+})).mockName("useAccount");
 
-export const useAccountInfo = fn(() => ({
+export const useAccountInfo: Mock<() => UseAccountInfoResponse> = fn(() => ({
   name: accounts["test-0"].username,
   address: accounts["test-0"].address,
   wallet: null,
   isFetching: false,
   error: "",
   warning: "",
-})).mockName("useAccountInfo") as Mock<() => UseAccountInfoResponse>;
+})).mockName("useAccountInfo");
