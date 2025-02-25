2a676bd400d495b634c86454e689b0df56620581
nrmnqdds <quddusnuriman@gmail.com>
2025-02-26 01:22:00 +0800

    chore: device type conditional rendering

--- packages/ui-next/src/components/modules/settings/signer-card.tsx
+++ packages/ui-next/src/components/modules/settings/signer-card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { cn } from "@/utils";
 import { Button, Card, CardContent } from "@/index";
-import { TrashIcon, MobileIcon } from "@/index";
+import { TrashIcon, MobileIcon, DesktopIcon, LaptopIcon } from "@/index";
 
 type DeviceType = "mobile" | "laptop" | "desktop";
 
@@ -13,7 +13,7 @@ interface SignerCardProps {
 }
 
 export const SignerCard = React.forwardRef<HTMLDivElement, SignerCardProps>(
-  ({ className, deviceName }, ref) => {
+  ({ className, deviceName, deviceType, onDelete }, ref) => {
     return (
       <div
         ref={ref}
@@ -21,13 +21,13 @@ export const SignerCard = React.forwardRef<HTMLDivElement, SignerCardProps>(
       >
         <Card className="flex flex-row items-center bg-background-100 border border-background-200">
           <CardContent className="relative bg-background-200 size-10 flex items-center justify-center">
-            <MobileIcon variant="solid" size="default" className="absolute" />
+            {renderDeviceType(deviceType)}
           </CardContent>
           <CardContent className="bg-background-100 py-2.5 px-3">
             <h1 className="flex-1 text-sm font-normal">{deviceName}</h1>
           </CardContent>
         </Card>
-        <Button variant="icon" size="icon">
+        <Button variant="icon" size="icon" type="button" onClick={onDelete}>
           <TrashIcon size="default" className="text-foreground-300" />
         </Button>
       </div>
@@ -36,3 +36,13 @@ export const SignerCard = React.forwardRef<HTMLDivElement, SignerCardProps>(
 );
 
 SignerCard.displayName = "SignerCard";
+
+const renderDeviceType = (deviceType: DeviceType) => {
+  return deviceType === "mobile" ? (
+    <MobileIcon variant="solid" size="default" className="absolute" />
+  ) : deviceType === "laptop" ? (
+    <LaptopIcon variant="solid" size="default" className="absolute" />
+  ) : (
+    <DesktopIcon variant="solid" size="default" className="absolute" />
+  );
+};
