fdb85169931dfc6ae5dd7ba8acd0a326c867dc8d
bal7hazar <bal7hazar@proton.me>
2025-02-26 19:09:36 +0100

    🐛 Revert workaround for opacity on buttons

--- packages/ui-next/src/components/primitives/button/index.tsx
+++ packages/ui-next/src/components/primitives/button/index.tsx
@@ -31,24 +31,22 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ) => {
     const Comp = asChild ? Slot : "button";
     return (
-      <div className="bg-[#000000] p-0 rounded">
-        <Comp
-          className={cn(
-            buttonVariants({
-              variant,
-              size,
-              status: isActive ? "active" : undefined,
-              className,
-            }),
-          )}
-          ref={ref}
-          disabled={disabled || isLoading}
-          {...props}
-        >
-          {isLoading ? <Spinner /> : children}
-          {variant === "link" && !isLoading && <ExternalIcon size="sm" />}
-        </Comp>
-      </div>
+      <Comp
+        className={cn(
+          buttonVariants({
+            variant,
+            size,
+            status: isActive ? "active" : undefined,
+            className,
+          }),
+        )}
+        ref={ref}
+        disabled={disabled || isLoading}
+        {...props}
+      >
+        {isLoading ? <Spinner /> : children}
+        {variant === "link" && !isLoading && <ExternalIcon size="sm" />}
+      </Comp>
     );
   },
 );
