40af80c3bb17238da83b5e72eea0b27638c21bd8
JunichiSugiura <jun.sugiura.jp@gmail.com>
2025-02-28 17:04:00 +0900

    Add connection mock creator

--- packages/keychain/src/components/transaction/ConfirmTransaction.stories.tsx
+++ packages/keychain/src/components/transaction/ConfirmTransaction.stories.tsx
@@ -3,44 +3,47 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { ConfirmTransaction } from "./ConfirmTransaction";
 import { ETH_CONTRACT_ADDRESS } from "@cartridge/utils";
 import { VerifiableControllerTheme } from "#components/provider/connection";
+import { useConnection, createMockConnection } from "#hooks/connection.mock";
 
 const meta = {
   component: ConfirmTransaction,
-  parameters: {
-    connection: {
-      controller: {
-        estimateInvokeFee: () => ({
-          suggestedMaxFee: "100",
-        }),
-        hasSession: () => true,
-        session: () => true,
-      },
-      context: {
-        origin: "http://localhost:6001",
-        type: "execute",
-        transactions: [
-          {
-            contractAddress: ETH_CONTRACT_ADDRESS,
-            entrypoint: "approve",
-            calldata: [
-              "0x0000000000000000000000000000000000000000000000000000000000000000",
-              "0x0",
-              "0x0",
-            ],
-          },
-          {
-            contractAddress: ETH_CONTRACT_ADDRESS,
-            entrypoint: "transfer",
-            calldata: [
-              "0x0000000000000000000000000000000000000000000000000000000000000000",
-              "0x0",
-              "0x0",
-            ],
-          },
-        ],
-        onCancel: () => {},
-      },
-    },
+  beforeEach: () => {
+    useConnection.mockReturnValue(
+      createMockConnection({
+        controller: {
+          estimateInvokeFee: () => ({
+            suggestedMaxFee: "100",
+          }),
+          hasSession: () => true,
+          session: () => true,
+        },
+        context: {
+          origin: "http://localhost:6001",
+          type: "execute",
+          transactions: [
+            {
+              contractAddress: ETH_CONTRACT_ADDRESS,
+              entrypoint: "approve",
+              calldata: [
+                "0x0000000000000000000000000000000000000000000000000000000000000000",
+                "0x0",
+                "0x0",
+              ],
+            },
+            {
+              contractAddress: ETH_CONTRACT_ADDRESS,
+              entrypoint: "transfer",
+              calldata: [
+                "0x0000000000000000000000000000000000000000000000000000000000000000",
+                "0x0",
+                "0x0",
+              ],
+            },
+          ],
+          onCancel: () => {},
+        },
+      }),
+    );
   },
 } satisfies Meta<typeof ConfirmTransaction>;
 
