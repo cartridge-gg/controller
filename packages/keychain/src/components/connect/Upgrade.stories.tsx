025f97a78be369ea0833a7696a0458b25c6335d6
JunichiSugiura <jun.sugiura.jp@gmail.com>
2025-02-28 16:10:19 +0900

    Mock upgrade

--- packages/keychain/src/components/connect/Upgrade.stories.tsx
+++ packages/keychain/src/components/connect/Upgrade.stories.tsx
@@ -1,8 +1,21 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Upgrade } from "./Upgrade";
+import {
+  useUpgrade,
+  createMockUpgrade,
+} from "#components/provider/upgrade.mock";
 
 const meta = {
   component: Upgrade,
+  beforeEach: () => {
+    useUpgrade.mockReturnValue(
+      createMockUpgrade({
+        latest: {
+          changes: ["Update 1", "Update 2", "Update 3"],
+        },
+      }),
+    );
+  },
 } satisfies Meta<typeof Upgrade>;
 
 export default meta;
