d49eeb4e5727745e7d0598f2d03e6c519d86ff3c
nrmnqdds <quddusnuriman@gmail.com>
2025-03-03 21:51:15 +0800

    chore: format

--- packages/keychain/src/components/settings/index.tsx
+++ packages/keychain/src/components/settings/index.tsx
@@ -65,7 +65,7 @@ const registeredAccounts: RegisteredAccount[] = [
 export function Settings() {
   const { logout, closeModal } = useConnection();
   const [state, setState] = useState<State>(State.SETTINGS);
-   
+
   const handleLogout = useCallback(() => {
     logout();
     closeModal();
