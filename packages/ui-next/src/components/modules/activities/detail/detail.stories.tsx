daa550bf8bf1562f4bece50a398f5694c5d4f9ea
bal7hazar <bal7hazar@proton.me>
2025-03-03 12:04:13 +0100

    ✨ Add detail row component and header and fix color constructive hex value

---
+++ packages/ui-next/src/components/modules/activities/detail/detail.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { ActivityDetail } from ".";
+import { CoinsIcon, StarknetColorIcon } from "@/components/icons";
+
+const meta: Meta<typeof ActivityDetail> = {
+  title: "Modules/Activities/Detail",
+  component: ActivityDetail,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "padded",
+  },
+  args: {
+    label: "Label",
+    children: <p>Content</p>,
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof ActivityDetail>;
+
+export const Default: Story = {};
+
+export const Samples: Story = {
+  render: () => (
+    <div className="flex flex-col gap-3 p-3">
+      <ActivityDetail label="Contract Address">0xb668...dd65</ActivityDetail>
+      <ActivityDetail label="To">0xb668...dd65</ActivityDetail>
+      <ActivityDetail label="From">0xb668...dd65</ActivityDetail>
+      <ActivityDetail label="Network">
+        <div className="flex gap-1.5 items-center">
+          <StarknetColorIcon />
+          Starknet
+        </div>
+      </ActivityDetail>
+      <ActivityDetail label="Network Fee">
+        <div className="flex gap-1.5 items-center">
+          <CoinsIcon variant="line" />
+          .01 CREDITS
+        </div>
+      </ActivityDetail>
+      <ActivityDetail label="Token ID">8</ActivityDetail>
+      <ActivityDetail label="Token Standard">ERC-721</ActivityDetail>
+      <ActivityDetail label="Date">Feb 11th, 2025 at 11:09 pm</ActivityDetail>
+      <ActivityDetail label="Status" status="success">
+        Succeeded
+      </ActivityDetail>
+    </div>
+  ),
+};
--- packages/ui-next/src/components/modules/activities/index.ts
+++ packages/ui-next/src/components/modules/activities/index.ts
@@ -1,1 +1,3 @@
 export * from "./card";
+export * from "./detail";
+export * from "./header";
