f05bce404affadab3c8e5523d60748ffda372a67
bal7hazar <bal7hazar@proton.me>
2025-03-13 16:43:22 +0100

    🔥 Remove unsupported svg rules

--- packages/ui-next/src/components/icons/utility/slash.tsx
+++ packages/ui-next/src/components/icons/utility/slash.tsx
@@ -13,8 +13,6 @@ export const SlashIcon = memo(
         {...props}
       >
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
           d="M4.10876 14L9.46582 1H10.8178L5.46074 14H4.10876Z"
           fill="currentColor"
         />
