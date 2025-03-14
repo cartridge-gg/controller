87201c73215910d183634391a8e07498bb77f26d
bal7hazar <bal7hazar@proton.me>
2025-03-14 17:02:23 +0100

    ✨ Add collectible components

---
+++ packages/ui-next/src/components/modules/collectibles/header/header.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { CollectibleHeader } from "./header";
+import { fn } from "@storybook/test";
+
+const meta: Meta<typeof CollectibleHeader> = {
+  title: "Modules/Collectibles/Header",
+  component: CollectibleHeader,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "padded",
+  },
+  args: {
+    title: "Beasts",
+    label: "9",
+    onSelect: fn(),
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof CollectibleHeader>;
+
+export const Default: Story = {};
+
+export const Hover: Story = {
+  args: {
+    hover: true,
+  },
+};
+
+export const Selectable: Story = {
+  args: {
+    selectable: true,
+  },
+};
+
+export const Selected: Story = {
+  args: {
+    selected: true,
+  },
+};
