992efffac38c0ca6253c1f7d188b717c64513806
broody <broody@gmail.com>
2025-03-25 14:39:35 -1000

    fix lib exports

--- packages/controller/tsup.node.config.ts
+++ packages/controller/tsup.node.config.ts
@@ -4,10 +4,12 @@ export default defineConfig({
   entry: ['src/node/index.ts'],
   outDir: 'dist/node',
   format: ['esm', 'cjs'], 
-  target: 'node16', 
   platform: 'node',
   splitting: false,
   sourcemap: true,
-  clean: false, 
-  dts: false, 
+  clean: true, 
+  dts: true, 
+  "treeshake": {
+    "preset": "recommended"
+  },
 });
