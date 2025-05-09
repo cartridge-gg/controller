46497782dc48d51096fa350e738ab63122bb074f
Valentin Dosimont <valentin@pupucecorp.com>
2025-05-09 15:45:51 +0200

    feat: remove ui package

--- packages/ui/src/components/icons/badge/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./bronze";
-export * from "./bronze-tag";
-export * from "./default";
-export * from "./gold";
-export * from "./gold-tag";
-export * from "./silver";
-export * from "./silver-tag";
--- packages/ui/src/components/layout/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./bottom-tabs";
-export * from "./container";
-export * from "./content";
-export * from "./empty";
-export * from "./footer";
-export * from "./header";
