21141b2e04cda782b34b0488664be392a68c57de
Tarrence van As <tarrence@vanas.family>
2025-07-07 11:38:54 -0400

    Initial profile migration

---
+++ packages/keychain/src/profile/components/achievements/index.tsx
@@ -0,0 +1,117 @@
+import {
+  LayoutContainer,
+  LayoutContent,
+  LayoutHeader,
+  Empty,
+  Skeleton,
+} from "@cartridge/ui";
+import { useAccount } from "#profile/hooks/account";
+import { useEffect, useMemo } from "react";
+import { useParams } from "react-router-dom";
+import { Trophies } from "./trophies";
+import { useConnection, useData } from "#profile/hooks/context";
+import { useArcade } from "#profile/hooks/arcade";
+import { EditionModel, GameModel } from "@cartridge/arcade";
+import { addAddressPadding } from "starknet";
+import { LayoutBottomNav } from "#profile/components/bottom-nav";
+
+export function Achievements() {
+  const { address: self } = useAccount();
+  const {
+    trophies: { achievements, players, status },
+    setAccountAddress,
+  } = useData();
+
+  const { pins, games, editions } = useArcade();
+
+  const { address } = useParams<{ address: string }>();
+  const { project, namespace } = useConnection();
+
+  const edition: EditionModel | undefined = useMemo(() => {
+    return Object.values(editions).find(
+      (edition) => edition.config.project === project,
+    );
+  }, [editions, project, namespace]);
+
+  const game: GameModel | undefined = useMemo(() => {
+    return Object.values(games).find((game) => game.id === edition?.gameId);
+  }, [games, edition]);
+
+  const pinneds = useMemo(() => {
+    const ids = (
+      pins[addAddressPadding(address || self || "0x0")] || []
+    ).filter((id) => achievements.find((item) => item.id === id)?.completed);
+    const pinneds = achievements
+      .filter(
+        (item) => item.completed && (ids.length === 0 || ids.includes(item.id)),
+      )
+      .sort((a, b) => a.id.localeCompare(b.id))
+      .sort((a, b) => parseFloat(a.percentage) - parseFloat(b.percentage))
+      .slice(0, 3); // There is a front-end limit of 3 pinneds
+    return pinneds;
+  }, [achievements, pins, address, self]);
+
+  const points = useMemo(() => {
+    return (
+      players.find((player) => player.address === (address || self))
+        ?.earnings || 0
+    );
+  }, [address, self, players]);
+
+  useEffect(() => {
+    setAccountAddress(address || self || "");
+  }, [address, self, setAccountAddress]);
+
+  return (
+    <LayoutContainer>
+      <LayoutHeader variant="hidden" />
+      {status === "loading" ? (
+        <LoadingState />
+      ) : status === "error" || !achievements.length ? (
+        <EmptyState />
+      ) : (
+        <LayoutContent className="py-6 gap-y-6 select-none h-full">
+          <Trophies
+            achievements={achievements}
+            pinneds={pinneds}
+            softview={false}
+            enabled={pinneds.length < 3}
+            game={game}
+            edition={edition}
+            pins={pins}
+            earnings={points}
+          />
+        </LayoutContent>
+      )}
+      <LayoutBottomNav />
+    </LayoutContainer>
+  );
+}
+
+const LoadingState = () => {
+  return (
+    <LayoutContent className="gap-y-4 select-none h-full overflow-hidden">
+      <div className="flex justify-between">
+        <Skeleton className="min-h-[136px] w-[120px] rounded" />
+        <Skeleton className="min-h-[136px] w-[120px] rounded" />
+        <Skeleton className="min-h-[136px] w-[120px] rounded" />
+      </div>
+      <Skeleton className="min-h-10 w-full rounded" />
+      {Array.from({ length: 10 }).map((_, index) => (
+        <Skeleton key={index} className="min-h-[177px] w-full rounded" />
+      ))}
+    </LayoutContent>
+  );
+};
+
+const EmptyState = () => {
+  return (
+    <LayoutContent className="select-none h-full">
+      <Empty
+        title="No achievements exist for this game."
+        icon="achievement"
+        className="h-full"
+      />
+    </LayoutContent>
+  );
+};
---
+++ packages/keychain/src/profile/components/inventory/collection/collectible.tsx
@@ -0,0 +1,136 @@
+import {
+  Link,
+  Outlet,
+  useLocation,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
+import {
+  LayoutContainer,
+  LayoutContent,
+  LayoutHeader,
+  CollectibleCard,
+  Skeleton,
+  Empty,
+} from "@cartridge/ui";
+import { cn } from "@cartridge/ui/utils";
+import { useCallback, useMemo } from "react";
+import placeholder from "/public/placeholder.svg";
+import { CollectionHeader } from "./header";
+import { useConnection, useTheme } from "#profile/hooks/context.js";
+import { useCollectible } from "#profile/hooks/collectible.js";
+import { useArcade } from "#profile/hooks/arcade.js";
+import { EditionModel, GameModel } from "@cartridge/arcade";
+
+export function Collectible() {
+  const { games, editions } = useArcade();
+  const { address: contractAddress, tokenId } = useParams();
+  const { closable, visitor, project, namespace } = useConnection();
+  const { theme } = useTheme();
+
+  const edition: EditionModel | undefined = useMemo(() => {
+    return Object.values(editions).find(
+      (edition) => edition.config.project === project,
+    );
+  }, [editions, project, namespace]);
+
+  const game: GameModel | undefined = useMemo(() => {
+    return Object.values(games).find((game) => game.id === edition?.gameId);
+  }, [games, edition]);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const { collectible, assets, status } = useCollectible({ contractAddress });
+
+  const handleBack = useCallback(() => {
+    navigate(`..?${searchParams.toString()}`);
+  }, [navigate, searchParams]);
+
+  if (tokenId || location.pathname.includes("/send")) {
+    return <Outlet />;
+  }
+
+  return (
+    <LayoutContainer>
+      <LayoutHeader
+        className="hidden"
+        onBack={closable || visitor ? undefined : handleBack}
+      />
+      {status === "loading" || !collectible || !assets ? (
+        <LoadingState />
+      ) : status === "error" ? (
+        <EmptyState />
+      ) : (
+        <>
+          <LayoutContent className={cn("p-6 flex flex-col gap-y-4")}>
+            <CollectionHeader
+              image={edition?.properties.icon || theme?.icon}
+              title={collectible.name}
+              subtitle={game?.name || theme?.name || "---"}
+              certified
+            />
+
+            <div className="grid grid-cols-2 gap-4 place-items-center">
+              {assets.map((asset) => {
+                return (
+                  <Link
+                    className="w-full select-none"
+                    draggable={false}
+                    to={`token/${asset.tokenId}?${searchParams.toString()}`}
+                    state={location.state}
+                    key={asset.tokenId}
+                  >
+                    <CollectibleCard
+                      title={
+                        asset.name.includes(
+                          `${parseInt(BigInt(asset.tokenId).toString())}`,
+                        )
+                          ? asset.name
+                          : `${asset.name} #${parseInt(BigInt(asset.tokenId).toString())}`
+                      }
+                      selectable={false}
+                      image={asset.imageUrl || placeholder}
+                      totalCount={asset.amount}
+                      className="rounded overflow-hidden"
+                    />
+                  </Link>
+                );
+              })}
+            </div>
+          </LayoutContent>
+        </>
+      )}
+    </LayoutContainer>
+  );
+}
+
+const LoadingState = () => {
+  return (
+    <LayoutContent className="gap-y-[52px] select-none h-full overflow-hidden">
+      <Skeleton className="min-h-10 w-full rounded" />
+      <div className="grid grid-cols-2 gap-4 place-items-center">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <Skeleton
+            key={index}
+            className="min-h-[168px] w-full rounded border-2 border-transparent"
+          />
+        ))}
+      </div>
+      <Skeleton className="min-h-10 w-full rounded" />
+    </LayoutContent>
+  );
+};
+
+const EmptyState = () => {
+  return (
+    <LayoutContent className="select-none h-full">
+      <Empty
+        title="No assets owned for this collection."
+        icon="inventory"
+        className="h-full"
+      />
+    </LayoutContent>
+  );
+};
