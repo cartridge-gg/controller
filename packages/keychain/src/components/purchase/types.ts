474812b84861fde0ba1a5a1f4a314c62a2c3e373
Yun <broody@gmail.com>
2025-07-07 14:18:36 -1000

    Refactor purchase flow (#1808)

---
+++ packages/keychain/src/components/purchase/types.ts
@@ -0,0 +1,30 @@
+import { ExternalWallet } from "@cartridge/controller";
+import { PurchaseType } from "@/hooks/payments/crypto";
+import { StarterPackDetails } from "@/hooks/starterpack";
+
+export enum PurchaseState {
+  SELECTION = 0,
+  STRIPE_CHECKOUT = 1,
+  CRYPTO_CHECKOUT = 2,
+  SUCCESS = 3,
+}
+
+export type PurchaseCreditsProps = {
+  isSlot?: boolean;
+  wallets?: ExternalWallet[];
+  type: PurchaseType;
+  starterpackDetails?: StarterPackDetails;
+  initState?: PurchaseState;
+  onBack?: () => void;
+};
+
+export type PricingDetails = {
+  baseCostInCents: number;
+  processingFeeInCents: number;
+  totalInCents: number;
+};
+
+export type StripeResponse = {
+  clientSecret: string;
+  pricing: PricingDetails;
+};
