b9a90d7ca2cd2ca555001eb6f9335a28e92de85c
Tarrence van As <tarrence@vanas.family>
2025-07-25 15:36:15 -0400

    Couple ui tweaks

--- packages/keychain/src/components/connect/create/Legal.tsx
+++ packages/keychain/src/components/connect/create/Legal.tsx
@@ -1,10 +1,8 @@
-import { LockIcon } from "@cartridge/ui";
 import { Link } from "react-router-dom";
 
 export function Legal() {
   return (
-    <div className="flex items-center gap-1 text-foreground-400">
-      <LockIcon />
+    <div className="flex items-center gap-1 text-foreground-400 px-1">
       <div className="text-xs">
         By continuing you are agreeing to Cartridge&apos;s{" "}
         <Link
