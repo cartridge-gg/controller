1ce1257d247b58e4c7de7bcc7ad850f0a9f92e32
Yun <broody@gmail.com>
2025-08-18 14:02:15 -1000

    feat: add purchase and claim starterpacks to next example (#1918)

---
+++ examples/next/src/components/Starterpack.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useAccount, useNetwork } from "@starknet-react/core";
+import ControllerConnector from "@cartridge/connector/controller";
+import { Button } from "@cartridge/ui";
+import { useEffect, useState } from "react";
+import { constants, num } from "starknet";
+
+export const Starterpack = () => {
+  const { account, connector } = useAccount();
+  const [purchaseSpId, setPurchaseSpId] = useState<string | null>(null);
+  const { chain } = useNetwork();
+
+  const controllerConnector = connector as unknown as ControllerConnector;
+
+  useEffect(() => {
+    if (!account) {
+      return;
+    }
+
+    if (num.toHex(chain.id) === constants.StarknetChainId.SN_MAIN) {
+      setPurchaseSpId("sick-starterpack-mainnet");
+    } else {
+      setPurchaseSpId("sick-starterpack-sepolia");
+    }
+  }, [chain, account]);
+
+  if (!account || !purchaseSpId) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col gap-2">
+      <h2>Starterpacks</h2>
+      <div className="flex flex-row gap-2">
+        <div className="flex items-center gap-2">
+          <Button
+            onClick={() => {
+              controllerConnector.controller.openStarterPack(purchaseSpId);
+            }}
+          >
+            Purchase Starterpack
+          </Button>
+        </div>
+        <div className="flex items-center gap-2">
+          <Button
+            onClick={() => {
+              controllerConnector.controller.openStarterPack(
+                "claim-starter-pack",
+              );
+            }}
+            disabled={true} // TODO: merkle proof claim flow
+          >
+            Claim Starterpack
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
