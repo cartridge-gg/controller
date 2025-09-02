db97ed4add3f5a3a09a61d71c07fd39fee25621e
tedison <timothyedison@hotmail.com>
2025-09-02 09:16:26 +0200

    Reapply "feat: controller creation factory pattern (#1891) and chore: update @cartridge/ui (#1953)"  (#1954)
    This reverts commit 60aa1a3cc92b2b6c7b732322c6382a3398865a8b.

--- packages/keychain/src/components/connect/create/webauthn/index.ts
+++ packages/keychain/src/components/connect/create/webauthn/index.ts
@@ -1,12 +1,12 @@
 import { DEFAULT_SESSION_DURATION, now } from "@/constants";
 import { doLogin, doSignup } from "@/hooks/account";
 import { useConnection } from "@/hooks/connection";
-import { Owner, Signer } from "@cartridge/controller-wasm";
+import Controller from "@/utils/controller";
+import { Owner } from "@cartridge/controller-wasm";
 import { ControllerQuery } from "@cartridge/ui/utils/api/cartridge";
 import { useCallback } from "react";
 import { shortString } from "starknet";
 import { LoginMode } from "../../types";
-import { createController } from "../useCreateController";
 
 export function useWebauthnAuthentication() {
   const { origin, rpcUrl, chainId, setController } = useConnection();
@@ -44,14 +44,14 @@ export function useWebauthnAuthentication() {
       if (!controllerNode || !finalUsername || !chainId || !rpcUrl || !origin)
         return;
 
-      const controller = await createController(
-        origin,
-        chainId,
+      const controller = await Controller.create({
+        appId: origin,
+        classHash: controllerNode.constructorCalldata[0],
+        chainId: shortString.decodeShortString(chainId),
         rpcUrl,
-        finalUsername,
-        controllerNode.constructorCalldata[0],
-        controllerNode.address,
-        {
+        address: controllerNode.address,
+        username: finalUsername,
+        owner: {
           signer: {
             webauthn: {
               rpId: import.meta.env.VITE_RP_ID!,
@@ -60,7 +60,7 @@ export function useWebauthnAuthentication() {
             },
           },
         },
-      );
+      });
 
       window.controller = controller;
       setController(controller);
@@ -71,43 +71,61 @@ export function useWebauthnAuthentication() {
   const login = useCallback(
     async (
       controller: ControllerQuery["controller"],
-      webauthnsSigner: Signer,
+      webauthnsSigner: Owner,
       loginMode: LoginMode,
       isSlot: boolean,
     ) => {
       if (!controller) throw new Error("No controller found");
+      if (!chainId) throw new Error("No chainId found");
 
-      const initialOwner: Owner = {
-        signer: {
-          webauthn: {
-            rpId: import.meta.env.VITE_RP_ID!,
-            credentialId: webauthnsSigner.webauthns?.[0]?.credentialId ?? "",
-            publicKey: webauthnsSigner.webauthns?.[0]?.publicKey ?? "",
-          },
-        },
-      };
-      const controllerObject = await createController(
-        origin!,
-        chainId!,
-        rpcUrl!,
-        controller.accountID,
-        controller.constructorCalldata[0],
-        controller.address,
-        initialOwner,
-      );
-
+      let controllerObject: Controller;
       if (loginMode === LoginMode.Webauthn) {
+        const webauthnCredential = webauthnsSigner.signer?.webauthns?.[0];
+        if (
+          !webauthnCredential ||
+          !webauthnCredential.publicKey ||
+          !webauthnCredential.credentialId
+        ) {
+          throw new Error("WebAuthn credentials are missing");
+        }
+
         await doLogin({
           name: controller.accountID,
-          credentialId: initialOwner.signer?.webauthn?.credentialId ?? "",
+          credentialId: webauthnCredential.credentialId,
           finalize: !!isSlot,
         });
+
+        controllerObject = Controller.create({
+          appId: origin,
+          classHash: controller.constructorCalldata[0],
+          chainId,
+          rpcUrl: rpcUrl,
+          address: controller.address,
+          username: controller.accountID,
+          owner: {
+            signer: {
+              webauthn: {
+                rpId: webauthnCredential.rpId || import.meta.env.VITE_RP_ID!,
+                credentialId: webauthnCredential.credentialId,
+                publicKey: webauthnCredential.publicKey,
+              },
+            },
+          },
+        });
       } else {
-        await controllerObject.login(
-          now() + DEFAULT_SESSION_DURATION,
-          true,
-          webauthnsSigner,
-        );
+        const { controller: loginController } = await Controller.login({
+          appId: origin,
+          classHash: controller.constructorCalldata[0],
+          rpcUrl,
+          chainId,
+          address: controller.address,
+          username: controller.accountID,
+          owner: webauthnsSigner,
+          cartridgeApiUrl: import.meta.env.VITE_CARTRIDGE_API_URL,
+          session_expires_at_s: Number(now() + DEFAULT_SESSION_DURATION),
+          isControllerRegistered: true,
+        });
+        controllerObject = loginController;
       }
 
       window.controller = controllerObject;
