d2d4f6d2c92a38d8210a29251ea1975e8e17c74f
broody <broody@gmail.com>
2025-09-12 08:22:31 -1000

    rm policy

--- examples/next/src/components/providers/StarknetProvider.tsx
+++ examples/next/src/components/providers/StarknetProvider.tsx
@@ -169,7 +169,6 @@ if (process.env.NEXT_PUBLIC_RPC_LOCAL) {
 }
 
 const controller = new ControllerConnector({
-  policies,
   // With the defaults, you can omit chains if you want to use:
   // - chains: [
   //     { rpcUrl: "https://api.cartridge.gg/x/starknet/sepolia/rpc/v0_9" },
