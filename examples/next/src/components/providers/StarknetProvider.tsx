eff6acf5d2f248532c455aafdc206766d1988ff3
broody <broody@gmail.com>
2025-09-12 02:38:29 -1000

    ls

--- examples/next/src/components/providers/StarknetProvider.tsx
+++ examples/next/src/components/providers/StarknetProvider.tsx
@@ -189,9 +189,7 @@ const controller = new ControllerConnector({
     "rabby",
     "password",
   ],
-  slot: "arcade-pistols",
-  namespace: "pistols",
-  preset: "pistols",
+  preset: "loot-survivor",
   // By default, preset policies take precedence over manually provided policies
   // Set shouldOverridePresetPolicies to true if you want your policies to override preset
   // shouldOverridePresetPolicies: true,
