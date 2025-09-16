179c4c3cf671652d72867f1ffc65dc7b5ce152d5
nrmnqdds <quddusnuriman@gmail.com>
2025-09-16 17:39:46 +0800

    chore: cleanup cost breakdown component

--- packages/keychain/src/components/purchasenew/review/cost.tsx
+++ packages/keychain/src/components/purchasenew/review/cost.tsx
@@ -1,6 +1,7 @@
 import {
   Card,
   CardContent,
+  cn,
   CreditIcon,
   InfoIcon,
   Thumbnail,
@@ -9,6 +10,7 @@ import { CostDetails } from "../types";
 import { ExternalPlatform, ExternalWalletType } from "@cartridge/controller";
 import { FeesTooltip } from "./tooltip";
 import { WALLET_CONFIG } from "@/components/purchase/CryptoCheckout";
+import React, { ComponentProps } from "react";
 
 type PaymentRails = "stripe" | "crypto";
 type PaymentUnit = "usdc" | "credits";
@@ -17,63 +19,75 @@ export const convertCentsToDollars = (cents: number): string => {
   return `$${(cents / 100).toFixed(2)}`;
 };
 
-export function CostBreakdown({
-  rails,
-  costDetails,
-  walletType,
-  platform,
-  paymentUnit,
-  openFeesTooltip = false,
-}: {
+export interface CostBreakdownProps {
   rails: PaymentRails;
   costDetails?: CostDetails;
   walletType?: ExternalWalletType;
   platform?: ExternalPlatform;
   paymentUnit?: PaymentUnit;
   openFeesTooltip?: boolean;
-}) {
-  if (rails === "crypto" && !walletType) {
-    return;
-  }
-
-  if (!costDetails) {
-    return <></>;
-  }
-
-  return (
-    <Card className="gap-3">
-      {rails === "crypto" && platform && (
-        <CardContent className="flex flex-col gap-2 border border-background-200 bg-[#181C19] rounded-[4px] text-xs text-foreground-400">
-          <div className="text-foreground-400 font-normal text-xs flex flex-row items-center gap-1">
-            Purchase on <Network walletType={walletType} />
-          </div>
-        </CardContent>
-      )}
-
-      <div className="flex flex-row gap-3 h-[40px]">
-        <CardContent className="flex items-center border border-background-200 bg-[#181C19] rounded-[4px] text-xs text-foreground-400 w-full">
-          <div className="flex justify-between text-sm font-medium w-full">
-            <div className="flex flex-row items-center gap-1">
-              <span>Total</span>
-              {costDetails.processingFeeInCents > 0 && (
-                <FeesTooltip
-                  trigger={<InfoIcon size="xs" />}
-                  isStripe={rails === "stripe"}
-                  defaultOpen={openFeesTooltip}
-                  costDetails={costDetails}
-                />
-              )}
-            </div>
-            <span className="text-foreground-100">
-              {convertCentsToDollars(costDetails.totalInCents)}
-            </span>
-          </div>
-        </CardContent>
-        <PaymentType unit={paymentUnit} />
-      </div>
-    </Card>
-  );
 }
+
+export const CostBreakdown = React.forwardRef<
+  HTMLDivElement,
+  ComponentProps<typeof Card> & CostBreakdownProps
+>(
+  (
+    {
+      className,
+      rails,
+      costDetails,
+      walletType,
+      platform,
+      paymentUnit,
+      openFeesTooltip = false,
+      ...props
+    },
+    ref,
+  ) => {
+    if (rails === "crypto" && !walletType) {
+      return;
+    }
+
+    if (!costDetails) {
+      return <></>;
+    }
+
+    return (
+      <Card ref={ref} className={cn("gap-3", className)} {...props}>
+        {rails === "crypto" && platform && (
+          <CardContent className="flex flex-col gap-2 border border-background-200 bg-[#181C19] rounded-[4px] text-xs text-foreground-400">
+            <div className="text-foreground-400 font-normal text-xs flex flex-row items-center gap-1">
+              Purchase on <Network walletType={walletType} />
+            </div>
+          </CardContent>
+        )}
+
+        <div className="flex flex-row gap-3 h-[40px]">
+          <CardContent className="flex items-center border border-background-200 bg-[#181C19] rounded-[4px] text-xs text-foreground-400 w-full">
+            <div className="flex justify-between text-sm font-medium w-full">
+              <div className="flex flex-row items-center gap-1">
+                <span>Total</span>
+                {costDetails.processingFeeInCents > 0 && (
+                  <FeesTooltip
+                    trigger={<InfoIcon size="xs" />}
+                    isStripe={rails === "stripe"}
+                    defaultOpen={openFeesTooltip}
+                    costDetails={costDetails}
+                  />
+                )}
+              </div>
+              <span className="text-foreground-100">
+                {convertCentsToDollars(costDetails.totalInCents)}
+              </span>
+            </div>
+          </CardContent>
+          <PaymentType unit={paymentUnit} />
+        </div>
+      </Card>
+    );
+  },
+);
 
 const PaymentType = ({ unit }: { unit?: PaymentUnit }) => {
   if (!unit) {
