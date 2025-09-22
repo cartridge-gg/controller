fc87bef308eba22e0ee699137c61dd9ea61e74b2
Daniel Kronovet <kronovet@gmail.com>
2025-09-22 15:47:07 -0400

    feat: Implement ControllerWithStarknetFeatures

--- examples/next/src/components/DelegateAccount.tsx
+++ examples/next/src/components/DelegateAccount.tsx
@@ -3,7 +3,7 @@
 import { useAccount } from "@starknet-react/core";
 import { useCallback, useEffect, useState } from "react";
 import { constants } from "starknet";
-import ControllerConnector from "@cartridge/connector/controller";
+import { ControllerConnector } from "@cartridge/connector";
 import { Button, Input } from "@cartridge/ui";
 
 export const DelegateAccount = () => {
