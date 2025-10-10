74b44c5356c31aa7d9b7efcb3b9b06f7a7e626cc
nrmnqdds <quddusnuriman@gmail.com>
2025-10-10 23:46:09 +0800

    style: update token contract card

--- packages/keychain/src/components/session/UnverifiedSessionSummary.tsx
+++ packages/keychain/src/components/session/UnverifiedSessionSummary.tsx
@@ -5,7 +5,13 @@ import { ContractCard } from "./ContractCard";
 import { MessageCard } from "./MessageCard";
 import { useMemo } from "react";
 import { TokenConsent } from "../connect/token-consent";
-import { TokenContractCard } from "./TokenContractCard";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  Thumbnail,
+} from "@cartridge/ui";
 
 export function UnverifiedSessionSummary({
   contracts,
@@ -64,17 +70,36 @@ export function UnverifiedSessionSummary({
       {tokenContracts && tokenContracts.length > 0 && (
         <>
           <TokenConsent />
-          {tokenContracts.map((e) => (
-            <TokenContractCard
-              key={e.address}
-              title={e.title}
-              icon={e.icon}
-              amount={
-                e.methods.find((method) => method.entrypoint === "approve")
-                  ?.amount ?? "0"
-              }
-            />
-          ))}
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between h-10">
+              <CardTitle className="normal-case font-semibold text-xs">
+                Spending Limit
+              </CardTitle>
+            </CardHeader>
+
+            {tokenContracts.map(({ title, icon, methods, address }) => {
+              const amount =
+                methods.find((m) => m.entrypoint === "approve")?.amount ?? "0";
+
+              return (
+                <CardContent
+                  key={address}
+                  className="flex flex-row gap-3 p-3 w-full"
+                >
+                  <Thumbnail icon={icon} size="md" variant="lighter" rounded />
+                  <div className="flex flex-col w-full">
+                    <div className="w-full flex flex-row items-center justify-between text-sm font-medium text-foreground-100">
+                      <p>{title}</p>
+                      <p>{Number(amount)}</p>
+                    </div>
+                    <p className="text-foreground-400 text-xs font-medium">
+                      {Number(amount)}
+                    </p>
+                  </div>
+                </CardContent>
+              );
+            })}
+          </Card>
         </>
       )}
     </div>
