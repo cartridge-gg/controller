e8232a7948c5dcbb731b0d5a1fb78f02e372527a
broody <broody@gmail.com>
2026-01-14 10:49:30 -1000

    check mark

--- packages/keychain/src/components/purchasenew/pending/confirming-transaction.tsx
+++ packages/keychain/src/components/purchasenew/pending/confirming-transaction.tsx
@@ -1,4 +1,10 @@
-import { Card, CardDescription, ExternalIcon, Spinner } from "@cartridge/ui";
+import {
+  Card,
+  CardDescription,
+  ExternalIcon,
+  Spinner,
+  CheckIcon,
+} from "@cartridge/ui";
 
 export function ConfirmingTransaction({
   title,
@@ -14,7 +20,11 @@ export function ConfirmingTransaction({
       <CardDescription className="flex flex-row items-start gap-3 items-center">
         <div className="flex justify-between w-full">
           <div className="text-foreground-200 font-normal text-xs flex items-center gap-1">
-            {isLoading && <Spinner size="sm" />}
+            {isLoading ? (
+              <Spinner size="sm" />
+            ) : (
+              <CheckIcon size="xs" className="text-success" />
+            )}
             {title}
           </div>
           {externalLink && (
