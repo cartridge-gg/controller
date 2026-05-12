7494ec7e9e3a54c94f7ca12e9e09b167c7893e2d
RM3 <roger@studioavante.com>
2026-05-12 15:21:57 -0300

    fix: Log In, not Login

--- packages/keychain/src/components/connect/create/password/PasswordForm.tsx
+++ packages/keychain/src/components/connect/create/password/PasswordForm.tsx
@@ -66,7 +66,7 @@ export function PasswordFormDrawer({
   return (
     <Drawer isOpen={isOpen} onClose={handleClose}>
       <DrawerContent
-        title={`${isLogin ? "Login" : "Sign Up"} with Password`}
+        title={`${isLogin ? "Log In" : "Sign Up"} with Password`}
         icon={<KeyIcon variant="solid" />}
       >
         <form onSubmit={handleSubmit} className="flex flex-col gap-4">
@@ -136,7 +136,7 @@ export function PasswordFormDrawer({
               id="primary-button"
               isLoading={isLoading}
             >
-              {isLogin ? "Login" : "Sign Up"}
+              {isLogin ? "Log In" : "Sign Up"}
             </Button>
           </div>
         </form>
