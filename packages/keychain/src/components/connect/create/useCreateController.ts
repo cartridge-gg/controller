87975cb069f06c488caf9133ddf941ffa7d2b811
RM3 <roger@studioavante.com>
2026-05-12 12:12:39 -0300

    fix: sms validation, turnkey upgrade

--- packages/keychain/src/components/connect/create/useCreateController.ts
+++ packages/keychain/src/components/connect/create/useCreateController.ts
@@ -230,6 +230,7 @@ export function useCreateController({
   const [smsState, setSmsState] = useState<{
     phoneNumber: string;
     otpId: string;
+    otpEncryptionTargetBundle: string;
   } | null>(null);
 
   const [authMethod, setAuthMethod] = useState<AuthOption | undefined>(
@@ -286,9 +287,10 @@ export function useCreateController({
     async (phoneNumber: string) => {
       try {
         setError(undefined);
-        setSmsState({ phoneNumber, otpId: "" });
-        const { otpId } = await smsAuth.initSms(phoneNumber);
-        setSmsState({ phoneNumber, otpId });
+        setSmsState({ phoneNumber, otpId: "", otpEncryptionTargetBundle: "" });
+        const { otpId, otpEncryptionTargetBundle } =
+          await smsAuth.initSms(phoneNumber);
+        setSmsState({ phoneNumber, otpId, otpEncryptionTargetBundle });
       } catch (e: unknown) {
         setError(e as Error);
       }
@@ -545,6 +547,7 @@ export function useCreateController({
             username,
             smsState.phoneNumber,
             smsState.otpId,
+            smsState.otpEncryptionTargetBundle,
             password,
           );
           signer = {
@@ -896,6 +899,7 @@ export function useCreateController({
             username,
             smsState.phoneNumber,
             smsState.otpId,
+            smsState.otpEncryptionTargetBundle,
             password,
           );
           loginResponse = { signer: smsResult.signer };
