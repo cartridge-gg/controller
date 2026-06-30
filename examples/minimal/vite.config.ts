564032cf576da4766dce8f37cbcfdc092651d3b3
RM3 <roger@studioavante.com>
2026-06-30 14:09:52 -0300

    feat: new minimal example

---
+++ examples/minimal/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+
+export default defineConfig({
+  plugins: [react(), wasm(), topLevelAwait()],
+});
